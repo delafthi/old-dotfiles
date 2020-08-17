1836e1ee92886426ff6c32cd638679dd35bd9823
Thierry Delafontaine <deaa@zhaw.ch>
2020-08-17 13:30:28 +0200

    Add further packages necessary for doom config

--- doom/.doom.d/install_script.sh
+++ doom/.doom.d/install_script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo apt install rtags pyhton glslang-tools jq libclang-dev pyhton3-pip python-setuptools
+sudo apt install rtags python glslang-tools jq libclang-dev python3-pip python-setuptools fd-find ripgrep cmake clang shellcheck pandoc
 pip3 install black pyflakes isort pipenv nose pytest
