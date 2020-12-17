a9bea4358d8e6a09fe0a5c6f73616213edeebef8
Thierry Delafontaine <deaa@zhaw.ch>
2020-12-17 10:41:32 +0100

    config.fish: Escaped wildcards in extract function ex()

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -177,35 +177,35 @@ function ex --description "Function to extract most types of archives"
         switch $argv
             case --help
                 echo "usage: ex <file>"
-            case *.tar.bz2
-                tar xjf $argv
-            case *.tar.gz
-                tar xzf $argv
-            case *.bz2
+            case '*.tar.bz2'
+                tar -xjf $argv
+            case '*.tar.gz'
+                tar -xzf $argv
+            case '*.bz2'
                 bunzip2 $argv
-            case *.rar
-                unrar x $argv
-            case *.gz
+            case '*.rar'
+                unrar -x $argv
+            case '*.gz'
                 gunzip $argv
-            case *.tar
-                tar xf $argv
-            case *.tbz2
-                tar xjf $argv
-            case *.tgz
-                tar xzf $argv
-            case *.zip
+            case '*.tar'
+                tar -xf $argv
+            case '*.tbz2'
+                tar -xjf $argv
+            case '*.tgz'
+                tar -xzf $argv
+            case '*.zip'
                 unzip $argv
-            case *.Z
+            case '*.Z'
                 uncompress $argv
-            case *.7z
-                7z x $argv
-            case *.deb
-                tar x $argv
-            case *.tar.xz
-                tar xf $argv
-            case *.tar.zst
+            case '*.7z'
+                7z -x $argv
+            case '*.deb'
+                tar -x $argv
+            case '*.tar.xz'
+                tar -xf $argv
+            case '*.tar.zst'
                 unzstd $argv
-            case *
+            case '*'
                 echo "'$argv' cannot be extracted via ex"
         end
     else
