6c27a778cb91afc43ecfdd364e9435f902fc16e3
Thierry Delafontaine <delafontaineth@pm.me>
2020-12-23 14:33:37 +0100

    config.el: Changed font to Victor Mono Nerd Font Mono.

--- doom/.doom.d/config.el
+++ doom/.doom.d/config.el
@@ -24,8 +24,8 @@
 ;; font string. You generally only need these two:
 ;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
-(setq doom-font (font-spec :family "RobotoMono Nerd Font" :size 16 :weight 'semi-light)
-      doom-variable-pitch-font (font-spec :family "RobotoMono Nerd Font" :size 17 :weight 'semi-light))
+(setq doom-font (font-spec :family "VictorMono Nerd Font Mono" :size 16)
+      doom-variable-pitch-font (font-spec :family "VictorMono Nerd Font Mono" :size 17))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
