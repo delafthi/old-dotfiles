4a351357ddf9e06d00fe1049ef45fd14956cdb30
Thierry Delafontaine <delafontaineth@pm.me>
2020-12-27 15:32:17 +0100

    treesitter/init.lua: Added a basic treesitter config.

---
+++ nvim/.config/nvim/lua/treesitter/init.lua
@@ -0,0 +1,6 @@
+--------------------------------------------------------------------------------
+-- Treesitter config
+--------------------------------------------------------------------------------
+
+local ts = require 'nvim-treesitter.configs'
+ts.setup{ensure_installed = 'maintained', highlight = {enable = true}}
