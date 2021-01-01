30889652d52e80666416fc8e0069c85b278bde4f
Thierry Delafontaine <delafontaineth@pm.me>
2021-01-01 01:48:43 +0100

    rc.lua: Removed action to minimize the active window when clicking on the tasklist.

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -238,19 +238,6 @@ end
 
 -- Tasklist widget
 local tasklist_buttons = gears.table.join(
-    awful.button({ }, 1,
-        function(c)
-            if c == client.focus then
-                c.minimized = true
-            else
-                c:emit_signal(
-                    "request::activate",
-                    "tasklist",
-                    {raise = true}
-                    )
-            end
-        end
-        ),
     awful.button({ }, 3,
         function() awful.menu.client_list({ theme = { width = 250 } }) end
         ),
