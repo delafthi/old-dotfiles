f92f25568972b257236293b840b6b99585022077
Thierry Delafontaine <delafontaineth@pm.me>
2021-01-15 12:10:12 +0100

    lsp_config/init.lua: Updated lsp config.

--- nvim/.config/nvim/lua/lsp_config/init.lua
+++ nvim/.config/nvim/lua/lsp_config/init.lua
@@ -2,29 +2,79 @@
 -- LSP config
 --------------------------------------------------------------------------------
 
---------------------------------------------------------------------------------
--- Local functions declaration
-local function map(mode, lhs, rhs, opts)
-  local options = {noremap = true}
-  if opts then options = vim.tbl_extend('force', options, opts) end
-  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+local lsp = require('lspconfig')
+local on_attach = function(client, bufnr)
+    local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+    local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+    buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+    -- Mappings.
+    local opts = { noremap=true, silent=true }
+    buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+    buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+    buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+    buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+    buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+    buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+    buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+    buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+    buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+    buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+    buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+    buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+    buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+    buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+    buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+
+    -- Set some keybinds conditional on server capabilities
+    if client.resolved_capabilities.document_formatting then
+      buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+    elseif client.resolved_capabilities.document_range_formatting then
+      buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+    end
+    -- Set autocommands conditional on server_capabilities
+    if client.resolved_capabilities.document_highlight then
+      require('lspconfig').util.nvim_multiline_command [[
+        augroup lsp_document_highlight
+          autocmd!
+          autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+          autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+        augroup END
+      ]]
+    end
 end
 
 --------------------------------------------------------------------------------
+-- Sign Character customization
+local sign_chars = vim.api.nvim_exec(
+    [[
+    sign define LspDiagnosticsSignError text= texthl=LspDiagnosticsSignError linehl= numhl=LspDiagnosticsSignError
+    sign define LspDiagnosticsSignWarning text= texthl=LspDiagnosticsSignWarning linehl= numhl=LspDiagnosticsSignWarning
+    sign define LspDiagnosticsSignInformation text= texthl=LspDiagnosticsSignInformation linehl= numhl=LspDiagnosticsSignInformation
+    sign define LspDiagnosticsSignHint text=ﯦ texthl=LspDiagnosticsSignHint linehl= numhl=LspDiagnosticsSignHint
+    ]],
+    true
+)
+
+--------------------------------------------------------------------------------
 -- Setup different language servers
-local lsp = require 'lspconfig'
 -- bash-language-server
-lsp.bashls.setup{}
+lsp.bashls.setup{ on_attach = on_attach }
 -- c-language-server
-lsp.ccls.setup{}
+lsp.ccls.setup{ on_attach = on_attach }
 -- dockerfile-language-server
-lsp.dockerls.setup{}
+lsp.dockerls.setup{ on_attach = on_attach }
 -- python-language-server
-lsp.pyls.setup{root_dir = lsp.util.root_pattern('.git', vim.fn.getcwd())}
+lsp.pyls.setup{
+    on_attach = on_attach,
+    root_dir = lsp.util.root_pattern('.git', vim.fn.getcwd()),
+}
 -- sumneko lua-language-server
 local sumneko_lua_root_path = vim.fn.stdpath('cache') .. '/lspconfig/sumneko_lua/lua-language-server'
 local sumneko_lua_binary = sumneko_lua_root_path .. '/bin/Linux/lua-language-server'
 lsp.sumneko_lua.setup{
+    on_attach = on_attach,
     cmd = {sumneko_lua_binary, '-E', sumneko_lua_root_path .. '/main.lua'};
     settings = {
         Lua = {
@@ -50,30 +100,6 @@ lsp.sumneko_lua.setup{
     },
 }
 -- (La)Tex-language-server
-lsp.texlab.setup{}
+lsp.texlab.setup{ on_attach = on_attach }
 -- vim-language-server
-lsp.vimls.setup{}
-
---------------------------------------------------------------------------------
--- Key mappings
-map('n', '<leader>,', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>')
-map('n', '<leader>;', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>')
-map('n', '<leader>a', '<cmd>lua vim.lsp.buf.code_action()<CR>')
-map('n', '<leader>d', '<cmd>lua vim.lsp.buf.definition()<CR>')
-map('n', '<leader>f', '<cmd>lua vim.lsp.buf.formatting()<CR>')
-map('n', '<leader>h', '<cmd>lua vim.lsp.buf.hover()<CR>')
-map('n', '<leader>m', '<cmd>lua vim.lsp.buf.rename()<CR>')
-map('n', '<leader>r', '<cmd>lua vim.lsp.buf.references()<CR>')
-map('n', '<leader>s', '<cmd>lua vim.lsp.buf.document_symbol()<CR>')
-
---------------------------------------------------------------------------------
--- Sign Character customization
-local sign_chars = vim.api.nvim_exec(
-    [[
-    sign define LspDiagnosticsSignError text= texthl=LspDiagnosticsSignError linehl= numhl=LspDiagnosticsSignError
-    sign define LspDiagnosticsSignWarning text= texthl=LspDiagnosticsSignWarning linehl= numhl=LspDiagnosticsSignWarning
-    sign define LspDiagnosticsSignInformation text= texthl=LspDiagnosticsSignInformation linehl= numhl=LspDiagnosticsSignInformation
-    sign define LspDiagnosticsSignHint text=ﯦ texthl=LspDiagnosticsSignHint linehl= numhl=LspDiagnosticsSignHint
-    ]],
-    true
-)
+lsp.vimls.setup{ on_attach = on_attach }
