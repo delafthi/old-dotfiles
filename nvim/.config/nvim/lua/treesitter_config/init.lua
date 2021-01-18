840c2158371edcc124320ae65f82c4393a932d19
Thierry Delafontaine <delafontaineth@pm.me>
2021-01-18 16:30:05 +0100

    treesitter_config/init.lua: Added inclompete config for vhdl treesitter.

--- nvim/.config/nvim/lua/treesitter_config/init.lua
+++ nvim/.config/nvim/lua/treesitter_config/init.lua
@@ -3,4 +3,13 @@
 --------------------------------------------------------------------------------
 
 local ts = require 'nvim-treesitter.configs'
-ts.setup{ensure_installed = 'maintained', highlight = {enable = true}}
+ts.setup{ensure_installed = 'all', highlight = {enable = true}}
+
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+parser_config.vhdl = {
+  install_info = {
+    url = "~/Projects/private/tree-sitter-vhdl",
+    files = { "src/parser.c" }
+  },
+  filetype = "vhd",
+}
