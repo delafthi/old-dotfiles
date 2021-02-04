2cdcad0812a420f99af8f353d662448e867ca9d0
Thierry Delafontaine <delafontaineth@pm.me>
2021-02-04 12:16:47 +0100

    autostart.sh: Removed programs called in .xinitrc.

--- qtile/.config/qtile/autostart.sh
+++ qtile/.config/qtile/autostart.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-xsetroot -cursor-name left_ptr &
-xss-lock slock -n &
-udiskie &
-picom &
-nm-applet &
-volumeicon &
 trayer --edge top --align right --distance 4,4 --distancefrom top,right --widthtype request --transparent true --height 24 --alpha 0 --tint 0x282c34 --padding 5 --monitor 0 --iconspacing 3 &
 dunst &
 feh --bg-scale --randomize /usr/share/backgrounds/* &
-pcmanfm -d &
-unclutter --timeout 10 &
