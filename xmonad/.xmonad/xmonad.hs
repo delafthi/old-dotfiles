31ef39f4acbe6bcdf65b6475d4a50cb1392d96a5
Thierry Delafontaine <delafontaineth@pm.me>
2021-02-04 12:17:21 +0100

    xmonad.hs: Removed startup programs called in .xinitrc.

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -388,20 +388,9 @@ myLogHook xmproc0 xmproc1 xmproc2 = workspaceHistoryHook <+> (dynamicLogWithPP .
 
 myStartupHook :: X ()
 myStartupHook = do
-    spawnOnce "xsetroot -cursor_name left_ptr &"
-    --spawnOnce "xrandr --output DP-1 --mode 2560x1440 --rate 143.97 --primary --output HDMI-0 --mode 2560x1440 --rate 60.00 --right-of DP-1 --output DP-1-1 --mode 2560x1440 --rate 120.00 --left-of DP-1 &"
-    spawnOnce "xrandr --output DP-1 --mode 2560x1440 --rate 143.97 --output HDMI-0 --auto --right-of DP-1 &"
-    spawnOnce "xss-lock slock &"
-    spawnOnce "udiskie &"
-    spawnOnce "picom &"
-    spawnOnce "nm-applet &"
-    spawnOnce "volumeicon &"
-    spawnOnce "blueman-applet &"
     spawnOnce "trayer --edge top --align right --distance 4,4 --distancefrom top,right --widthtype request --transparent true --height 24 --alpha 0 --tint 0x282c34 --padding 5 --monitor 0 --iconspacing 3 &"
     spawnOnce "dunst &"
     spawnOnce "feh --bg-scale --randomize /usr/share/backgrounds/*"
-    spawnOnce "pcmanfm -d &"
-    spawnOnce "unclutter --timeout 10 &"
     setWMName "LG3D"
 
 --------------------------------------------------------------------------------
