b5c326fd1028ae98065b273ccb5c4f02753be649
Thierry Delafontaine <delafontaineth@pm.me>
2021-02-08 23:28:25 +0100

    nvim-treesitter.lua: Renamed config.

--- nvim/.config/nvim/lua/config/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/config/nvim-treesitter.lua
@@ -4,12 +4,3 @@
 
 local ts = require 'nvim-treesitter.configs'
 ts.setup{ensure_installed = 'all', highlight = {enable = true}}
-
-local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-parser_config.vhdl = {
-  install_info = {
-    url = "~/Projects/private/tree-sitter-vhdl",
-    files = { "src/parser.c" }
-  },
-  filetype = "vhd",
-}
