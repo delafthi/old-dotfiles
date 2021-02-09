50f2cd2f2769cdf469dd37fe3898a2ad64f43c9f
Thierry Delafontaine <delafontaineth@pm.me>
2021-02-09 20:47:26 +0100

    kommntary.lua: Finalized config.

--- nvim/.config/nvim/lua/config/kommentary.lua
+++ nvim/.config/nvim/lua/config/kommentary.lua
@@ -1,14 +1,15 @@
 local M = {}
 
 function M.setup()
-	vim.g.kommentary_create_default_mappings = false
+  vim.g.kommentary_create_default_mappings = false
 end
 
 function M.config()
   require'kommentary.config'.use_extended_mappings()
-  vim.api.nvim_set_keymap("n", "<leader>cc", "<Plug>kommentary_line_default", {})
-  vim.api.nvim_set_keymap("n", "<leader>c", "<Plug>kommentary_motion_default", {})
-  vim.api.nvim_set_keymap("v", "<leader>c", "<Plug>kommentary_visual_default", {})
+  local opts = {silent = true}
+  vim.api.nvim_set_keymap('n', '<Leader>cc', '<Plug>kommentary_line_default', opts)
+  vim.api.nvim_set_keymap('n', '<Leader>c', '<Plug>kommentary_motion_default', opts)
+  vim.api.nvim_set_keymap('v', '<Leader>c', '<Plug>kommentary_visual_default', opts)
 end
 
 return M
