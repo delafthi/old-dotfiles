9c2bcd331bcd9c675fb426cd3e49d8e98eba0d29
Thierry Delafontaine <delafontaineth@pm.me>
2021-02-09 20:47:44 +0100

    markdown-preview.lua: Finalized config.

--- nvim/.config/nvim/lua/config/markdown-preview.lua
+++ nvim/.config/nvim/lua/config/markdown-preview.lua
@@ -1,4 +1,15 @@
-vim.g.mkdp_refresh_slow = 0
-vim.g.mkdp_browser = 'brave'
-vim.g.mkdp_page_title = '${name}'
-vim.api.nvim_set_keymap('n', '<Leader>mp', '<Plug>MarkdownPreviewToggle', nil)
+local M = {}
+
+function M.setup()
+  vim.g.mkdp_autoclose = 0 -- Disable auto close, when changing to a different buffer.
+  vim.g.mkdp_browser = 'brave' -- Set default browser.
+  vim.gmkdp_filetypes = {'markdown', 'vimwiki'} -- Set compatible filetypes.
+  vim.g.mkdp_page_title = '${name}' -- Set page title.
+  vim.g.mkdp_refresh_slow = 0 -- Turn on faust auto refresh.
+end
+
+function M.config()
+  vim.api.nvim_set_keymap('n', '<Leader>mp', '<Plug>MarkdownPreviewToggle', {silent = true}) -- Set keymap to toggle preview.
+end
+
+return M
