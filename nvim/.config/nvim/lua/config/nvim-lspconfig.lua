11fd43e0054f1a24a7547c4f4d33b00dc2b86626
Thierry Delafontaine <delafontaineth@pm.me>
2021-02-09 20:48:13 +0100

    nvim-lspconfig.lua: Finalized config.

--- nvim/.config/nvim/lua/config/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/config/nvim-lspconfig.lua
@@ -1,112 +1,112 @@
---------------------------------------------------------------------------------
--- LSP config
---------------------------------------------------------------------------------
-
+local M = {}
 local lsp = require('lspconfig')
-local on_attach = function(client, bufnr)
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
 
-  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+function M.setup()
+  local on_attach = function(client, bufnr)
+    local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+    local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
 
-  -- Mappings.
-  local opts = { noremap=true, silent=true }
-  buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  buf_set_keymap('n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  buf_set_keymap('n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  buf_set_keymap('n', '<leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-  buf_set_keymap('n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  buf_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  buf_set_keymap('n', '<leader>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-  buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-  buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-  buf_set_keymap('n', '<leader>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+    buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-  -- Set some keybinds conditional on server capabilities
-  if client.resolved_capabilities.document_formatting then
-    buf_set_keymap('n', '<leader>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
-  elseif client.resolved_capabilities.document_range_formatting then
-    buf_set_keymap('n', '<leader>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+    -- Mappings.
+    local opts = { noremap=true, silent=true }
+    buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<Cr>', opts)
+    buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<Cr>', opts)
+    buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<Cr>', opts)
+    buf_set_keymap('n', 'gi', '<Cmd>lua vim.lsp.buf.implementation()<Cr>', opts)
+    buf_set_keymap('n', '<C-k>', '<Cmd>lua vim.lsp.buf.signature_help()<Cr>', opts)
+    buf_set_keymap('n', '<Leader>wa', '<Cmd>lua vim.lsp.buf.add_workspace_folder()<Cr>', opts)
+    buf_set_keymap('n', '<Leader>wr', '<Cmd>lua vim.lsp.buf.remove_workspace_folder()<Cr>', opts)
+    buf_set_keymap('n', '<Leader>wl', '<Cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<Cr>', opts)
+    buf_set_keymap('n', '<Leader>D', '<Cmd>lua vim.lsp.buf.type_definition()<Cr>', opts)
+    buf_set_keymap('n', '<Leader>rn', '<Cmd>lua vim.lsp.buf.rename()<Cr>', opts)
+    buf_set_keymap('n', 'gr', '<Cmd>lua vim.lsp.buf.references()<Cr>', opts)
+    buf_set_keymap('n', '<Leader>e', '<Cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<Cr>', opts)
+    buf_set_keymap('n', '[d', '<Cmd>lua vim.lsp.diagnostic.goto_prev()<Cr>', opts)
+    buf_set_keymap('n', ']d', '<Cmd>lua vim.lsp.diagnostic.goto_next()<Cr>', opts)
+    buf_set_keymap('n', '<Leader>q', '<Cmd>lua vim.lsp.diagnostic.set_loclist()<Cr>', opts)
+
+    -- Set some keybinds conditional on server capabilities
+    if client.resolved_capabilities.document_formatting then
+      buf_set_keymap('n', '<leader>f', '<cmd>lua vim.lsp.buf.formatting()<Cr>', opts)
+    elseif client.resolved_capabilities.document_range_formatting then
+      buf_set_keymap('n', '<leader>f', '<cmd>lua vim.lsp.buf.formatting()<Cr>', opts)
+    end
+    -- Set autocommands conditional on server_capabilities
+    if client.resolved_capabilities.document_highlight then
+      vim.api.nvim_exec([[
+      augroup lsp_document_highlight
+        autocmd!
+        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+      augroup END
+      ]], false)
+    end
   end
-  -- Set autocommands conditional on server_capabilities
-  if client.resolved_capabilities.document_highlight then
-    vim.api.nvim_exec([[
-    augroup lsp_document_highlight
-      autocmd!
-      autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-      autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-    augroup END
-    ]], false)
-  end
-end
 
---------------------------------------------------------------------------------
--- Sign Character customization
-local sign_chars = vim.api.nvim_exec(
-  [[
-  sign define LspDiagnosticsSignError text= texthl=LspDiagnosticsSignError linehl= numhl=LspDiagnosticsSignError
-  sign define LspDiagnosticsSignWarning text= texthl=LspDiagnosticsSignWarning linehl= numhl=LspDiagnosticsSignWarning
-  sign define LspDiagnosticsSignInformation text= texthl=LspDiagnosticsSignInformation linehl= numhl=LspDiagnosticsSignInformation
-  sign define LspDiagnosticsSignHint text=ﯦ texthl=LspDiagnosticsSignHint linehl= numhl=LspDiagnosticsSignHint
-  ]],
-  true
-)
+  -- Sign Character customization
+  local sign_chars = vim.api.nvim_exec(
+    [[
+    sign define LspDiagnosticsSignError text= texthl=LspDiagnosticsSignError linehl= numhl=LspDiagnosticsSignError
+    sign define LspDiagnosticsSignWarning text= texthl=LspDiagnosticsSignWarning linehl= numhl=LspDiagnosticsSignWarning
+    sign define LspDiagnosticsSignInformation text= texthl=LspDiagnosticsSignInformation linehl= numhl=LspDiagnosticsSignInformation
+    sign define LspDiagnosticsSignHint text=ﯦ texthl=LspDiagnosticsSignHint linehl= numhl=LspDiagnosticsSignHint
+    ]],
+    true
+  )
 
---------------------------------------------------------------------------------
--- Setup different language servers
--- bash-language-server
-lsp.bashls.setup{ on_attach = on_attach }
--- c-language-server
-lsp.ccls.setup{ on_attach = on_attach }
--- dockerfile-language-server
-lsp.dockerls.setup{ on_attach = on_attach }
--- haskell-language-server
-lsp.hls.setup{
-  on_attach = on_attach,
-  root_dir = lsp.util.root_pattern('*.cabal', 'stack.yaml', 'cabal.project', 'package.yaml', 'hie.yaml')
-}
--- python-language-server
-lsp.pyls.setup{
-  on_attach = on_attach,
-  root_dir = lsp.util.root_pattern('.git', vim.fn.getcwd()),
-}
--- sumneko lua-language-server
-local sumneko_lua_root_path = vim.fn.stdpath('cache') .. '/lspconfig/sumneko_lua/lua-language-server'
-local sumneko_lua_binary = sumneko_lua_root_path .. '/bin/Linux/lua-language-server'
-lsp.sumneko_lua.setup{
-  on_attach = on_attach,
-  cmd = {sumneko_lua_binary, '-E', sumneko_lua_root_path .. '/main.lua'};
-  settings = {
-    Lua = {
-      runtime = {
-        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-        version = 'LuaJIT',
-        -- Setup your lua path
-        path = vim.split(package.path, ';'),
-      },
-      diagnostics = {
-        enable = true,
-        -- Get the language server to recognize the vim and awesome globals
-        globals = {'vim', 'awesome', 'client', 'root', 'screen'},
-      },
-      workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = {
-          [vim.fn.expand('$VIMRUNTIME/lua')] = true,
-          [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
-          },
+  -- Setup different language servers
+  -- bash-language-server
+  lsp.bashls.setup{ on_attach = on_attach }
+  -- c-language-server
+  lsp.ccls.setup{ on_attach = on_attach }
+  -- dockerfile-language-server
+  lsp.dockerls.setup{ on_attach = on_attach }
+  -- haskell-language-server
+  lsp.hls.setup{
+    on_attach = on_attach,
+    root_dir = lsp.util.root_pattern('*.cabal', 'stack.yaml', 'cabal.project', 'package.yaml', 'hie.yaml')
+  }
+  -- python-language-server
+  lsp.pyls.setup{
+    on_attach = on_attach,
+    root_dir = lsp.util.root_pattern('.git', vim.fn.getcwd()),
+  }
+  -- sumneko lua-language-server
+  local sumneko_lua_root_path = vim.fn.stdpath('cache') .. '/lspconfig/sumneko_lua/lua-language-server'
+  local sumneko_lua_binary = sumneko_lua_root_path .. '/bin/Linux/lua-language-server'
+  lsp.sumneko_lua.setup{
+    on_attach = on_attach,
+    cmd = {sumneko_lua_binary, '-E', sumneko_lua_root_path .. '/main.lua'};
+    settings = {
+      Lua = {
+        runtime = {
+          -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+          version = 'LuaJIT',
+          -- Setup your lua path
+          path = vim.split(package.path, ';'),
+        },
+        diagnostics = {
+          enable = true,
+          -- Get the language server to recognize the vim and awesome globals
+          globals = {'vim', 'awesome', 'client', 'root', 'screen'},
+        },
+        workspace = {
+          -- Make the server aware of Neovim runtime files
+          library = {
+            [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+            [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+            },
+        },
       },
     },
-  },
-}
--- (La)Tex-language-server
-lsp.texlab.setup{ on_attach = on_attach }
--- vim-language-server
-lsp.vimls.setup{ on_attach = on_attach }
--- yaml-language-server
-lsp.yamlls.setup{ on_attach = on_attach }
+  }
+  -- (La)Tex-language-server
+  lsp.texlab.setup{ on_attach = on_attach }
+  -- vim-language-server
+  lsp.vimls.setup{ on_attach = on_attach }
+  -- yaml-language-server
+  lsp.yamlls.setup{ on_attach = on_attach }
+end 
+
+return M
