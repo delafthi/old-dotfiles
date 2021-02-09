7b4e9682a394f383671497f604e6897140e38ba0
Thierry Delafontaine <delafontaineth@pm.me>
2021-02-09 20:49:09 +0100

    vimwiki.lua: Finalized config.

--- nvim/.config/nvim/lua/config/vimwiki.lua
+++ nvim/.config/nvim/lua/config/vimwiki.lua
@@ -1,9 +1,15 @@
-vim.g.vimwiki_list = {
-  {
-    path = '~/Vimwiki/',
-    syntax = 'markdown',
-    ext = '.md',
+local M = {}
+
+function M.setup()
+  vim.g.vimwiki_list = {
+    {
+      path = '~/Vimwiki/',
+      syntax = 'markdown',
+      ext = '.md',
+    }
   }
-}
-vim.g.vimwiki_use_mouse = 1
-vim.g.vimwiki_auto_dir = 1
+  vim.g.vimwiki_use_mouse = 1
+  vim.g.vimwiki_auto_dir = 1
+end
+
+return M
