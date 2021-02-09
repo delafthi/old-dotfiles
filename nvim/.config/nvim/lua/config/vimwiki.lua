c0b5b3201d3bdbdbc37242a114d88fcbde6eeb2d
Thierry Delafontaine <delafontaineth@pm.me>
2021-02-09 01:06:05 +0100

    vimwiki.lua: Fixed config.

--- nvim/.config/nvim/lua/config/vimwiki.lua
+++ nvim/.config/nvim/lua/config/vimwiki.lua
@@ -1,7 +1,9 @@
 vim.g.vimwiki_list = {
-  {path = '~/Vimwiki/'},
-  {syntax = 'markdown'},
-  {ext = '.md'},
+  {
+    path = '~/Vimwiki/',
+    syntax = 'markdown',
+    ext = '.md',
+  }
 }
 vim.g.vimwiki_use_mouse = 1
 vim.g.vimwiki_auto_dir = 1
