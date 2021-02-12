c2248c271990f55ed8fcf4bdf55ec1ef3c81105c
Thierry Delafontaine <delafontaineth@pm.me>
2021-02-12 18:27:18 +0100

    kommentary.lua: Changed keybindings.

--- nvim/.config/nvim/lua/config/kommentary.lua
+++ nvim/.config/nvim/lua/config/kommentary.lua
@@ -7,9 +7,9 @@ end
 function M.config()
   require'kommentary.config'.use_extended_mappings()
   local opts = {silent = true}
-  vim.api.nvim_set_keymap('n', '<Leader>cc', '<Plug>kommentary_line_default', opts)
-  vim.api.nvim_set_keymap('n', '<Leader>c', '<Plug>kommentary_motion_default', opts)
-  vim.api.nvim_set_keymap('v', '<Leader>c', '<Plug>kommentary_visual_default', opts)
+  vim.api.nvim_set_keymap('n', 'gcc', '<Plug>kommentary_line_default', opts)
+  vim.api.nvim_set_keymap('n', 'gc', '<Plug>kommentary_motion_default', opts)
+  vim.api.nvim_set_keymap('v', 'gc', '<Plug>kommentary_visual_default', opts)
 end
 
 return M
