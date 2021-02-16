ee299f64ef6974748af849d7a129faae36d911b7
Thierry Delafontaine <delafontaineth@pm.me>
2021-02-16 15:49:58 +0100

    nvim-tree.lua: Added some additional setting variables.

--- nvim/.config/nvim/lua/config/nvim-tree.lua
+++ nvim/.config/nvim/lua/config/nvim-tree.lua
@@ -16,6 +16,8 @@ function M.setup()
   vim.g.nvim_tree_root_folder_modifier = ':~'
   vim.g.nvim_tree_tab_open = 1
   vim.g.nvim_tree_width_allow_resize  = 1
+  vim.g.nvim_tree_disable_netrw = 1
+  vim.g.nvim_tree_hijack_netrw = 1
   vim.g.nvim_tree_show_icons = {
     git = 1,
     folders = 1,
