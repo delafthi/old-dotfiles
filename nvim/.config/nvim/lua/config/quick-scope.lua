ba094ae847fd3fdcf09143a482fb8c036405fade
Thierry Delafontaine <delafontaineth@pm.me>
2021-02-21 19:55:45 +0100

    quick-scope.lua: Added help files to blacklist.

--- nvim/.config/nvim/lua/config/quick-scope.lua
+++ nvim/.config/nvim/lua/config/quick-scope.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 function M.setup()
-  vim.g.qs_buftype_blacklist = {'terminal', 'nofile', 'NvimTree'}
+  vim.g.qs_buftype_blacklist = {'terminal', 'nofile', 'NvimTree', 'help'}
 end
 
 return M
