e40f32b0c682f3da1b224b8745ef836263d4f762
Thierry Delafontaine <delafontaineth@pm.me>
2021-02-21 20:24:24 +0100

    quick-scope.lua: Added color config to here instead of the colorscheme file.

--- nvim/.config/nvim/lua/config/quick-scope.lua
+++ nvim/.config/nvim/lua/config/quick-scope.lua
@@ -2,6 +2,13 @@ local M = {}
 
 function M.setup()
   vim.g.qs_buftype_blacklist = {'terminal', 'nofile', 'NvimTree', 'help'}
+  vim.api.nvim_exec([[
+  augroup qs_color
+    autocmd!
+    autocmd ColorScheme * highlight QuickScopePrimary gui=bold,underline cterm=bold,underline
+    autocmd ColorScheme * highlight QuickScopeSecondary gui=bold,underline cterm=bold,underline
+  augroup END
+  ]], false)
 end
 
 return M
