bf548a6e7fa4065632659645841519c396f2d217
Thierry Delafontaine <delafontaineth@pm.me>
2021-03-06 13:59:32 +0100

    Added some settings for the preview.

--- nvim/.config/nvim/lua/config/markdown-preview.lua
+++ nvim/.config/nvim/lua/config/markdown-preview.lua
@@ -7,6 +7,19 @@ function M.setup()
   vim.gmkdp_filetypes = {'markdown', 'vimwiki'} -- Set compatible filetypes.
   vim.g.mkdp_page_title = '${name}' -- Set page title.
   vim.g.mkdp_refresh_slow = 0 -- Turn on faust auto refresh.
+  vim.g.mkdp_preview_options = {
+    mkit = {},
+    katex = {},
+    uml = {},
+    disable_sync_scroll = 0,
+    sync_scroll_type = 'relative',
+    hide_yaml_meta = 1,
+    sequence_diagrams = {},
+    flowchart_diagrams = {},
+    content_editable = false,
+    disable_filename = 0,
+  }
+
 end
 
 function M.config()
