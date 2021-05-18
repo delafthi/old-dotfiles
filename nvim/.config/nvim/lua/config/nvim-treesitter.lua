21114c072dcb945e0692a96536688d80db95e26e
Thierry Delafontaine <delafontaineth@pm.me>
2021-05-18 16:01:16 +0200

    Minor changes to the tree-sitter config.

--- nvim/.config/nvim/lua/config/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/config/nvim-treesitter.lua
@@ -14,9 +14,20 @@ function M.config()
     highlight = {
       enable = true,
     },
+    indent = {
+      enable = true
+    },
+    keymaps = {
+      init_selection = "gnn",
+      node_incremental = "grn",
+      scope_incremental = "grc",
+      node_decremental = "grm",
+    },
+    -- Treesitter Plugins
     rainbow = {
       enable = true,
       extended_mode = true,
+      max_file_lines = 1000,
     },
   }
 end
