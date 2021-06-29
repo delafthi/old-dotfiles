37dd25a6ce33d3e64f72132ce213e4073b3ab6dc
Thierry Delafontaine <delafontaineth@pm.me>
2021-06-29 12:26:15 +0200

    Initial commit of a configuration for nord.nvim.

---
+++ nvim/.config/nvim/lua/config/nord.lua
@@ -0,0 +1,19 @@
+local M = {}
+
+function M.setup()
+  vim.g.nord_borders = true
+end
+
+function M.config()
+  local ok, nord = pcall(function()
+    return require('nord')
+  end)
+
+  if not ok then
+    return
+  end
+
+  nord.set()
+end
+
+return M
