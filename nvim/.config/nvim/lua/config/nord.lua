4d2c302d6c35fd31e9f3565417b0f88161a25b64
Thierry Delafontaine <delafontaineth@pm.me>
2021-08-29 19:46:42 +0200

    nvim: Added further settings to the nord theme config.

--- nvim/.config/nvim/lua/config/nord.lua
+++ nvim/.config/nvim/lua/config/nord.lua
@@ -1,6 +1,10 @@
 local M = {}
 
-function M.setup() vim.g.nord_borders = true end
+function M.setup()
+  vim.g.nord_borders = true
+  vim.g.nord_contrast = true
+  vim.g.nord_italic = true
+end
 
 function M.config()
   local ok, nord = pcall(function() return require('nord') end)
