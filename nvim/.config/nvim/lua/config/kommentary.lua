0a0cc6946f37e0d81d224996ead31573d194bf6f
Thierry Delafontaine <delafontaineth@pm.me>
2021-08-30 20:40:40 +0200

    nvim: Slight change to kommentary keybindings.

--- nvim/.config/nvim/lua/config/kommentary.lua
+++ nvim/.config/nvim/lua/config/kommentary.lua
@@ -11,8 +11,7 @@ function M.config()
 
   kommentary.use_extended_mappings()
   local opts = {noremap = false, silent = true}
-  u.map('n', 'gcc', '<Plug>kommentary_line_default', opts)
-  u.map('n', 'gc', '<Plug>kommentary_motion_default', opts)
+  u.map('n', 'gc', '<Plug>kommentary_line_default', opts)
   u.map('v', 'gc', '<Plug>kommentary_visual_default', opts)
 end
 
