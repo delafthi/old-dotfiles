bf4f8baee9fec95e5f3c146c6461e17761e1eee1
Thierry Delafontaine <delafontaineth@pm.me>
2021-08-30 21:11:33 +0200

    nvim: Added signs.

--- nvim/.config/nvim/lua/config/nvim-dap.lua
+++ nvim/.config/nvim/lua/config/nvim-dap.lua
@@ -2,31 +2,15 @@ local M = {}
 local u = require('utils')
 
 function M.config()
-  local ok, dap = pcall(function()
-    return require('dap')
-  end)
+  local ok, dap = pcall(function() return require('dap') end)
 
-  if not ok then
-    return
-  end
+  if not ok then return end
 
-
-  -- Mappings.
-  local opts = {noremap=true, silent=true}
-  u.map('n', '<Leader>b', '<Cmd>lua require("dap").toggle_breakpoint()<Cr>', opts)
-  u.map('n', '<Leader>dx', '<Cmd>lua require("dap").continue()<Cr>', opts)
-  u.map('n', '<Leader>sn', '<Cmd>lua require("dap").step_over()<Cr>', opts)
-  u.map('n', '<Leader>si', '<Cmd>lua require("dap").step_into()<Cr>', opts)
-  u.map('n', '<Leader>so', '<Cmd>lua require("dap").step_out()<Cr>', opts)
-  u.map('n', '<Leader>bl', '<Cmd>lua require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: "))<Cr>', opts)
-  u.map('n', '<Leader>dr', '<Cmd>lua require("dap").repl.toggle()<Cr>', opts)
-  u.map('n', '<Leader>vi', '<Cmd>lua require("dap.ui.widgets").hover()<Cr>', opts)
-
-
+  -- Configurations
   dap.adapters.lldb = {
     type = 'executable',
     command = '/usr/bin/lldb-vscode',
-    name = 'lldb',
+    name = 'lldb'
   }
   dap.configurations.cpp = {
     {
@@ -34,15 +18,49 @@ function M.config()
       type = 'lldb',
       request = 'launch',
       program = function()
-        return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+        return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/',
+                            'file')
       end,
       cwd = '${workspaceFolder}',
       stopOnEntry = false,
       args = {},
-      runInTerminal = false,
-    },
+      runInTerminal = false
+    }
   }
-dap.configurations.c = dap.configurations.cpp
+  dap.configurations.c = dap.configurations.cpp
+
+  -- Visuals
+  vim.fn.sign_define('DapBreakpoint',
+                     {text = ' ', texthl = 'Error', linehl = '', numhl = ''})
+  vim.fn.sign_define('DapBreakpointRejected', {
+    text = ' ',
+    texthl = 'Ignore',
+    linehl = '',
+    numhl = ''
+  })
+  vim.fn.sign_define('DapLogPoint',
+                     {text = 'ﯽ ', texthl = 'Todo', linehl = '', numhl = ''})
+  vim.fn.sign_define('DapStopped', {
+    text = ' ',
+    texthl = 'Warnings',
+    linehl = '',
+    numhl = ''
+  })
+
+  -- Mappings.
+  local opts = {noremap = true, silent = true}
+  u.map('n', '<Leader>b', '<Cmd>lua require("dap").toggle_breakpoint()<Cr>',
+        opts)
+  u.map('n', '<Leader>dx', '<Cmd>lua require("dap").continue()<Cr>', opts)
+  u.map('n', '<Leader>sn', '<Cmd>lua require("dap").step_over()<Cr>', opts)
+  u.map('n', '<Leader>si', '<Cmd>lua require("dap").step_into()<Cr>', opts)
+  u.map('n', '<Leader>so', '<Cmd>lua require("dap").step_out()<Cr>', opts)
+  u.map('n', '<Leader>bl',
+        '<Cmd>lua require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: "))<Cr>',
+        opts)
+  u.map('n', '<Leader>dr', '<Cmd>lua require("dap").repl.toggle()<Cr>', opts)
+  u.map('n', '<Leader>vi', '<Cmd>lua require("dap.ui.widgets").hover()<Cr>',
+        opts)
 
 end
 
