d8156d4183b994e67c8c7344f05909714e803b46
Thierry Delafontaine <deaa@zhaw.ch>
2021-09-03 13:51:22 +0200

    nvim: Forgot to add the config file to ca291a19727a6cc82629690ebf19ffff76b10ccf.

---
+++ nvim/.config/nvim/lua/config/dashboard.lua
@@ -0,0 +1,39 @@
+local M = {}
+local u = require('utils')
+
+function M.setup()
+  vim.g.dashboard_default_executive = 'telescope'
+  vim.g.dashboard_custom_header = {
+    '                                                       ',
+    '                                                       ',
+    ' ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗',
+    ' ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║',
+    ' ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║',
+    ' ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║',
+    ' ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║',
+    ' ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝',
+    '                                                       ',
+    '                                                       ',
+  }
+  --[[ vim.g.dashboard_custom_shortcut = {
+    last_session = 'SPC s l',
+    new_file = 'SPC c n',
+    find_file = 'SPC f f',
+    find_word = 'SPC r g',
+  }
+  vim.g.dashboard_custom_shortcut_icon = {
+    last_session = ' ',
+    new_file = ' ',
+    find_file = ' ',
+    find_word = ' ',
+  } ]]
+  vim.g.dashboard_custom_footer = {}
+
+  -- Keybindings
+  local opts = { noremap = true, silent = true }
+  u.map('n', '<Leader>ss', ':<C-u>SessionSave<Cr>', opts)
+  u.map('n', '<Leader>sl', ':<C-u>SessionLoad<Cr>', opts)
+  u.map('n', '<Leader>cn', ':DashboardNewFile<Cr>', opts)
+end
+
+return M
