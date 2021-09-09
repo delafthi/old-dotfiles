5f68f0467f4f1d751af00a74096e48404daeed1f
Thierry Delafontaine <delafontaineth@pm.me>
2021-09-09 19:16:41 +0200

    nvim: Updated configuration for stylua.

--- nvim/.config/nvim/lua/config/formatter.lua
+++ nvim/.config/nvim/lua/config/formatter.lua
@@ -2,117 +2,118 @@ local M = {}
 local u = require("utils")
 
 function M.config()
-	local ok, formatter = pcall(function()
-		return require("formatter")
-	end)
+  local ok, formatter = pcall(function()
+    return require("formatter")
+  end)
 
-	if not ok then
-		return
-	end
+  if not ok then
+    return
+  end
 
-	formatter.setup({
-		filetype = {
-			c = {
-				function()
-					return {
-						exe = "clang-format",
-						args = { "--assume-filename", vim.api.nvim_buf_get_name(0) },
-						stdin = true,
-						cwd = vim.fn.expand("%:p:h"),
-					}
-				end,
-			},
-			cpp = {
-				function()
-					return {
-						exe = "clang-format",
-						args = {
-							"--assume-filename",
-							vim.api.nvim_buf_get_name(0),
-						},
-						stdin = true,
-						cwd = vim.fn.expand("%:p:h"),
-					}
-				end,
-			},
-			fish = {
-				function()
-					return {
-						exe = "fish_indent",
-						stdin = true,
-					}
-				end,
-			},
-			haskell = {
-				function()
-					return {
-						exe = "hindent",
-						args = {
-							"--line-lenght",
-							80,
-							"--indent-size",
-							2,
-							"--sort-imports",
-						},
-						stdin = true,
-					}
-				end,
-			},
-			lua = {
-				function()
-					return {
-						exe = "stylua",
-						args = {
-							"--stdin-filepath",
-							vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)),
-							"-",
-						},
-						stdin = true,
-					}
-				end,
-			},
-			markdown = {
-				function()
-					return {
-						exe = "prettier",
-						args = { "-w" },
-						stdin = true,
-					}
-				end,
-			},
-			python = {
-				function()
-					return {
-						exe = "yapf",
-					}
-				end,
-			},
-			shell = {
-				function()
-					return {
-						exe = "shfmt",
-						args = {
-							"-ln",
-							"posix",
-							"-i",
-							2,
-							"-ci",
-							"-sr",
-							"-kp",
-							"-fn",
-						},
-						stdin = true,
-					}
-				end,
-			},
-		},
-	})
+  formatter.setup({
+    filetype = {
+      c = {
+        function()
+          return {
+            exe = "clang-format",
+            args = { "--assume-filename", vim.api.nvim_buf_get_name(0) },
+            stdin = true,
+            cwd = vim.fn.expand("%:p:h"),
+          }
+        end,
+      },
+      cpp = {
+        function()
+          return {
+            exe = "clang-format",
+            args = {
+              "--assume-filename",
+              vim.api.nvim_buf_get_name(0),
+            },
+            stdin = true,
+            cwd = vim.fn.expand("%:p:h"),
+          }
+        end,
+      },
+      fish = {
+        function()
+          return {
+            exe = "fish_indent",
+            stdin = true,
+          }
+        end,
+      },
+      haskell = {
+        function()
+          return {
+            exe = "hindent",
+            args = {
+              "--line-lenght",
+              80,
+              "--indent-size",
+              2,
+              "--sort-imports",
+            },
+            stdin = true,
+          }
+        end,
+      },
+      lua = {
+        function()
+          return {
+            exe = "stylua",
+            args = {
+              "--stdin-filepath",
+              vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)),
+              "--search-parent-directories",
+              "-",
+            },
+            stdin = true,
+          }
+        end,
+      },
+      markdown = {
+        function()
+          return {
+            exe = "prettier",
+            args = { "-w" },
+            stdin = true,
+          }
+        end,
+      },
+      python = {
+        function()
+          return {
+            exe = "yapf",
+          }
+        end,
+      },
+      shell = {
+        function()
+          return {
+            exe = "shfmt",
+            args = {
+              "-ln",
+              "posix",
+              "-i",
+              2,
+              "-ci",
+              "-sr",
+              "-kp",
+              "-fn",
+            },
+            stdin = true,
+          }
+        end,
+      },
+    },
+  })
 
-	local opts = { noremap = true, silent = true }
-	u.map("n", "<Leader>bf", ":Format<Cr>", opts)
+  local opts = { noremap = true, silent = true }
+  u.map("n", "<Leader>bf", ":Format<Cr>", opts)
 
-	-- Automatically formats filetypes on save.
-	vim.cmd([[
+  -- Automatically formats filetypes on save.
+  vim.cmd([[
   augroup format_text
     autocmd!
     autocmd BufWritePost * FormatWrite
