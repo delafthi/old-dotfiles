bb34c01c1bf0cd39c3b93b4a1c2c527539bd7938
Thierry Delafontaine <delafontaineth@pm.me>
2021-09-11 14:28:27 +0200

    nvim: Personalized nvim-dashboard.

--- nvim/.config/nvim/lua/config/dashboard.lua
+++ nvim/.config/nvim/lua/config/dashboard.lua
@@ -15,25 +15,42 @@ function M.setup()
     "                                                       ",
     "                                                       ",
   }
-  --[[ vim.g.dashboard_custom_shortcut = {
-    last_session = 'SPC s l',
-    new_file = 'SPC c n',
-    find_file = 'SPC f f',
-    find_word = 'SPC r g',
+
+  vim.g.dashboard_custom_section = {
+    restore_session = {
+      description = { " Restore last session                       SPC s l" },
+      command = "SessionLoad",
+    },
+    find_history = {
+      description = { " Recently opened files                      SPC h h" },
+      command = "DashboardFindHistory",
+    },
+    find_files = {
+      description = { " Find File                                  SPC f f" },
+      command = "Telescope find_files",
+    },
+    file_browser = {
+      description = { " File Browser                               SPC f b" },
+      command = "Telescope find_browser",
+    },
+    find_word = {
+      description = { " Find Word                                  SPC r g" },
+      command = "Telescope grep_string",
+    },
+    new_file = {
+      description = { " New File                                   SPC n f" },
+      command = "DashboardNewFile",
+    },
   }
-  vim.g.dashboard_custom_shortcut_icon = {
-    last_session = ' ',
-    new_file = ' ',
-    find_file = ' ',
-    find_word = ' ',
-  } ]]
+
   vim.g.dashboard_custom_footer = {}
 
   -- Keybindings
   local opts = { noremap = true, silent = true }
   u.map("n", "<Leader>ss", ":<C-u>SessionSave<Cr>", opts)
   u.map("n", "<Leader>sl", ":<C-u>SessionLoad<Cr>", opts)
-  u.map("n", "<Leader>cn", ":DashboardNewFile<Cr>", opts)
+  u.map("n", "<Leader>nf", ":DashboardNewFile<Cr>", opts)
+  u.map("n", "<Leader>hh", ":DashboardFindHistory<Cr>", opts)
 end
 
 return M
