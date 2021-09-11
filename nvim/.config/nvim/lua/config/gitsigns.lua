0ebd6ef46eac2ac06b2c5a88fc3bb05b45ea7bab
Thierry Delafontaine <delafontaineth@pm.me>
2021-09-11 14:27:52 +0200

    nvim: Removed all keybindings from gitsigns.

--- nvim/.config/nvim/lua/config/gitsigns.lua
+++ nvim/.config/nvim/lua/config/gitsigns.lua
@@ -48,28 +48,6 @@ function M.config()
       -- Default keymap options
       noremap = true,
       buffer = true,
-
-      ["n ]c"] = {
-        expr = true,
-        "&diff ? ']c' : '<cmd>lua require\"gitsigns.actions\".next_hunk()<CR>'",
-      },
-      ["n [c"] = {
-        expr = true,
-        "&diff ? '[c' : '<cmd>lua require\"gitsigns.actions\".prev_hunk()<CR>'",
-      },
-
-      ["n <Leader>hs"] = "<cmd>lua require('gitsigns').stage_hunk()<CR>",
-      ["v <Leader>hs"] = "<cmd>lua require('gitsigns').stage_hunk({vim.fn.line('.'), vim.fn.line('v')})<CR>",
-      ["n <Leader>hu"] = "<cmd>lua require('gitsigns').undo_stage_hunk()<CR>",
-      ["n <Leader>hr"] = "<cmd>lua require('gitsigns').reset_hunk()<CR>",
-      ["v <Leader>hr"] = "<cmd>lua require('gitsigns').reset_hunk({vim.fn.line('.'), vim.fn.line('v')})<CR>",
-      ["n <Leader>hR"] = "<cmd>lua require('gitsigns').reset_buffer()<CR>",
-      ["n <Leader>hp"] = "<cmd>lua require('gitsigns').preview_hunk()<CR>",
-      ["n <Leader>hb"] = "<cmd>lua require('gitsigns').blame_line(true)<CR>",
-
-      -- Text objects
-      ["o ih"] = ":<C-U>lua require('gitsigns.actions').select_hunk()<CR>",
-      ["x ih"] = ":<C-U>lua require('gitsigns.actions').select_hunk()<CR>",
     },
     watch_index = { interval = 1000 },
     current_line_blame = false,
