1818947ac1b323d623ac149c85389e14b9f7f960
Thierry Delafontaine <delafontaineth@pm.me>
2021-09-11 10:06:06 +0200

    nvim: Silenced output of formatter autocmd.

--- nvim/.config/nvim/lua/config/formatter.lua
+++ nvim/.config/nvim/lua/config/formatter.lua
@@ -116,7 +116,7 @@ function M.config()
   vim.cmd([[
   augroup format_text
     autocmd!
-    autocmd BufWritePost * FormatWrite
+    autocmd BufWritePost * silent! FormatWrite
   augroup END
   ]])
 end
