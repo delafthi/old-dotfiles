967565d14ee05eab1d2a67f49817ead966383477
Thierry Delafontaine <delafontaineth@pm.me>
2021-09-18 13:32:13 +0200

    bash/fish/bin: Create shell scripts instead of functions.
    Pull out functions defined in .bashrc and config.fish and create shell scripts instead.

--- bash/.bashrc
+++ bash/.bashrc
@@ -12,13 +12,13 @@
 bind "set completion-ignore-case on"
 
 # Bash options
-shopt -s autocd # change to named directory
-shopt -s cdspell # autocorrects cd misspellings
-shopt -s cmdhist # save multi-line commands in history as single line
-shopt -s dotglob # bash includes filenames beginning with a ‘.’ in the results of filename expansion
-shopt -s histappend # do not overwrite history
+shopt -s autocd         # change to named directory
+shopt -s cdspell        # autocorrects cd misspellings
+shopt -s cmdhist        # save multi-line commands in history as single line
+shopt -s dotglob        # bash includes filenames beginning with a ‘.’ in the results of filename expansion
+shopt -s histappend     # do not overwrite history
 shopt -s expand_aliases # expand aliases
-shopt -s checkwinsize # checks term size when bash regains control
+shopt -s checkwinsize   # checks term size when bash regains control
 
 # Set vi bindings
 set -o vi
@@ -42,9 +42,9 @@ alias .5="cd ../../../../.."
 alias :q="exit"
 
 # Adding flags
-alias cp="cp -i"                          # confirm before overwriting something
-alias df="df -h"                          # human-readable sizes
-alias free="free -m"                      # show sizes in MB
+alias cp="cp -i"     # confirm before overwriting something
+alias df="df -h"     # human-readable sizes
+alias free="free -m" # show sizes in MB
 alias rm="rm -i"
 alias mv="mv -i"
 alias minicom="minicom -m -c on"
@@ -54,22 +54,22 @@ alias feh="feh --auto-zoom --scale-down"
 ############################################################
 # Environment variables {{{1
 
-export HISTCONTROL=ignoreboth             # no duplicate entries
+export HISTCONTROL=ignoreboth # no duplicate entries
 export HISTSIZE=5000
 export HISTFILESIZE=10000
 # Set the default editor, this variable is overwritten in case neovim is
 # installed
 export EDITOR="vi"
-export SSH_KEY_PATH="~/.ssh/rsa_id"       # Set default ssh key path
+export SSH_KEY_PATH="~/.ssh/rsa_id" # Set default ssh key path
 export GCC_COLORS="error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01"
 
 # Path
 if [ -d "$HOME/.bin" ]; then
-    PATH="$HOME/.bin:$PATH"
+  PATH="$HOME/.bin:$PATH"
 fi
 
 if [ -d "$HOME/.local/bin" ]; then
-    PATH="$HOME/.local/bin:$PATH"
+  PATH="$HOME/.local/bin:$PATH"
 fi
 
 ############################################################
@@ -78,108 +78,28 @@ fi
 # Load dircolors
 test -r ~/.dir_colors && eval $(dircolors ~/.dir_colors)
 
-############################################################
-# Functions {{{1
-
-# Archive extraction
-function ex ()
-{
-  if [ -f $1 ]; then
-    case $1 in
-      --help)      echo "usage: ex <file>" ;;
-      *.tar.bz2)   tar xjf $1   ;;
-      *.tar.gz)    tar xzf $1   ;;
-      *.bz2)       bunzip2 $1   ;;
-      *.rar)       unrar x $1   ;;
-      *.gz)        gunzip $1    ;;
-      *.tar)       tar xf $1    ;;
-      *.tbz2)      tar xjf $1   ;;
-      *.tgz)       tar xzf $1   ;;
-      *.zip)       unzip $1     ;;
-      *.Z)         uncompress $1;;
-      *.7z)        7z x $1      ;;
-      *.deb)       ar x $1      ;;
-      *.tar.xz)    tar xf $1    ;;
-      *.tar.zst)   unzstd $1    ;;
-      *)           echo "'$1' cannot be extracted via ex()" ;;
-    esac
-  else
-    echo "'$1' is not a valid file"
-  fi
-}
-
 ###########################################################
 # Settings with dependencies {{{1
 
 # Set defaults for bat
-if command -v bat 1>/dev/null 2>&1 && command -v rg 1>/dev/null 2>&1; then
+if command -v bat 1> /dev/null 2>&1 && command -v rg 1> /dev/null 2>&1; then
   alias bat="bat --italic-text=always --color=always --theme Nord"
-
-  # Use ripgrep instead of grep and outputs via bat
-  function batgrep ()
-  {
-    rg $@ --hidden --color always | bat --paging=never --color=always
-  }
   alias grep="batgrep"
-
-  # Use ripgrep --files instead of find and use bat for the output
-  function batfind ()
-  {
-    rg $@ --ignore --color=always --smart-case --hidden --files | bat
-  }
   alias find="batfind"
-
-  # Use bat for a nicer git diff
-  function batdiff ()
-  {
-    git diff $@ --name-only --diff-filter=d | xargs bat --diff
-  }
 fi
 
 # Use fzf in combination with grep
 # fzf colors
-if command -v fzf 1>/dev/null 2>&1 && command -v rg 1>/dev/null 2>&1; then
+if command -v fzf 1> /dev/null 2>&1 && command -v rg 1> /dev/null 2>&1; then
   alias fzf="fzf \
     --color='fg:#d8dee9,bg:#2e3440,fg+:#81a1c1,bg+:#2e3440,border:#4c566a' \
     --color='info:#81a1c1,spinner:#b48ead,header:#bf616a,prompt:#b48ead' \
     --color='hl:#ebcb8b,hl+:#ebcb8b,pointer:#b48ead,marker:#d08770' \
     --color='fg+:reverse,header:bold,pointer:bold,marker:bold,prompt:bold' \
     --color='hl:reverse,hl+:reverse'"
-
-  export INITIAL_QUERY=""
-  export RG_PREFIX="rg --ignore --hidden --column --line-number --with-filename --no-heading --color=always --smart-case "
-  export FZF_DEFAULT_COMMAND="$RG_PREFIX '$INITIAL_QUERY'"
-  alias fg="fzf \
-    --bind 'change:reload:$RG_PREFIX {q} || true' \
-    --disabled \
-    --ansi \
-    --query '$INITIAL_QUERY' \
-    --height=50% \
-    --tabstop=2 \
-    --layout=reverse \
-    --nth=2 \
-    --delimiter : \
-    --preview-window '+{2}/2' \
-    --preview 'bat \
-    --style=numbers \
-    -r {2}: \
-    -H {2} \
-    --line-range :500 {1}'"
-  alias ff="rg \
-    --ignore \
-    --smart-case \
-    --hidden \
-    --files | fzf \
-    --ansi \
-    --height=50% \
-    --layout=reverse \
-    --tabstop=2 \
-    --preview 'bat \
-    --style=numbers \
-    --line-range :500 {}'"
 fi
 
-if command -v nvim 1>/dev/null 2>&1; then
+if command -v nvim 1> /dev/null 2>&1; then
   alias vim="nvim"
   alias vi="nvim"
   # set vim as manpager
@@ -187,7 +107,7 @@ if command -v nvim 1>/dev/null 2>&1; then
 fi
 
 # Changing "ls" to "exa"
-if command -v exa 1>/dev/null 2>&1; then
+if command -v exa 1> /dev/null 2>&1; then
   alias ls="exa -al --color=always --group-directories-first" # my preferred listing
   alias la="exa -a --color=always --group-directories-first"  # all files and dirs
   alias ll="exa -l --color=always --group-directories-first"  # long format
@@ -196,16 +116,16 @@ fi
 
 # kitty specific aliases
 if [ $TERM == "xterm-kitty" ]; then
-    alias ssh="kitty +kitten ssh"
+  alias ssh="kitty +kitten ssh"
 fi
 
 ############################################################
 # Plugins {{{1
 
 # Starship prompt
-if ! command -v starship 1>/dev/null 2>&1; then
-    echo -e "$COLOR_RED $BOLD => Error: $COLOR_RESET $NORMAL Starship not installed.\n"
-    echo -e "$COLOR_GREEN $BOLD => Info: $COLOR_RESET $NORMAL Please install starship through your package manager or manually. An installation guide can be found here: https://starship.rs/guide/#%F0%9F%9A%80-installation"
+if ! command -v starship 1> /dev/null 2>&1; then
+  echo -e "$COLOR_RED $BOLD => Error: $COLOR_RESET $NORMAL Starship not installed.\n"
+  echo -e "$COLOR_GREEN $BOLD => Info: $COLOR_RESET $NORMAL Please install starship through your package manager or manually. An installation guide can be found here: https://starship.rs/guide/#%F0%9F%9A%80-installation"
 else
   export STARSHIP_CONFIG="$HOME/.config/starship/starship.toml"
   eval "$(starship init bash)"
@@ -217,7 +137,7 @@ if [ -f /etc/profile.d/nix.sh ]; then
 fi
 
 # pyenv
-if command -v pyenv 1>/dev/null 2>&1; then
+if command -v pyenv 1> /dev/null 2>&1; then
   eval "$(pyenv init --path)"
   eval "$(pyenv init -)"
   if [ ! -d $PYENV_ROOT/plugins/pyenv-virtualenv ]; then
--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -10,19 +10,19 @@ set fish_greeting
 
 # Set window title
 function fish_title
-  echo $argv[1]
-  pwd
+    echo $argv[1]
+    pwd
 end
 
 # Set vim keybinding
 function fish_user_key_bindings
-  fish_vi_cursor
-  fish_vi_key_bindings
+    fish_vi_cursor
+    fish_vi_key_bindings
 end
 
 # Set command not found handler to the default one
 function fish_command_not_found
-  __fish_default_command_not_found_handler $argv
+    __fish_default_command_not_found_handler $argv
 end
 
 ############################################################
@@ -39,12 +39,12 @@ abbr -a .4 "cd ../../../.."
 abbr -a .5 "cd ../../../../.."
 
 # vim like exit
-abbr -a :q "exit"
+abbr -a :q exit
 
 # Adding flags
-alias cp="cp -i"                          # confirm before overwriting something
-alias df="df -h"                          # human-readable sizes
-alias free="free -m"                      # show sizes in MB
+alias cp="cp -i" # confirm before overwriting something
+alias df="df -h" # human-readable sizes
+alias free="free -m" # show sizes in MB
 alias rm="rm -i"
 alias mv="mv -i"
 alias minicom="minicom -m -c on"
@@ -56,17 +56,17 @@ alias feh="feh --auto-zoom --scale-down"
 
 # Set the default editor, this variable is overwritten in case neovim is
 # installed
-set -gx EDITOR "vi"
+set -gx EDITOR vi
 set -gx SSH_KEY_PATH "~/.ssh/rsa_id" # Set default ssh key path
 set -gx GCC_COLORS "error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01"
 
 # Path
 if test -d $HOME/.bin
-  set -gx PATH $HOME/.bin $PATH
+    set -gx PATH $HOME/.bin $PATH
 end
 
 if test -d $HOME/.local/bin
-  set -gx PATH $HOME/.local/bin $PATH
+    set -gx PATH $HOME/.local/bin $PATH
 end
 
 ############################################################
@@ -103,56 +103,12 @@ set fish_cursor_insert line
 set fish_cursor_replace_one underscore
 set fish_cursor_replace underscore
 
-############################################################
-# Functions {{{1
-
-function ex --description "Function to extract most types of archives"
-  if test -f $argv
-    switch $argv
-      case --help
-        echo "usage: ex <file>"
-      case '*.tar.bz2'
-        tar xjf $argv
-      case '*.tar.gz'
-        tar xzf $argv
-      case '*.bz2'
-        bunzip2 $argv
-      case '*.rar'
-        unrar x $argv
-      case '*.gz'
-        gunzip $argv
-      case '*.tar'
-        tar xf $argv
-      case '*.tbz2'
-        tar xjf $argv
-      case '*.tgz'
-        tar xzf $argv
-      case '*.zip'
-        unzip $argv
-      case '*.Z'
-        uncompress $argv
-      case '*.7z'
-        7z x $argv
-      case '*.deb'
-        tar x $argv
-      case '*.tar.xz'
-        tar xf $argv
-      case '*.tar.zst'
-        unzstd $argv
-      case '*'
-        echo "'$argv' cannot be extracted via ex"
-    end
-  else
-    echo "'$argv' is not a valid file"
-  end
-end
-
 function nofunc --description "Run command ignoring functions and aliases"
-  functions -c $argv[1] functionholder
-  functions --erase $argv[1]
-  $argv
-  functions -c functionholder $argv[1]
-  functions --erase functionholder
+    functions -c $argv[1] functionholder
+    functions --erase $argv[1]
+    $argv
+    functions -c functionholder $argv[1]
+    functions --erase functionholder
 end
 
 ############################################################
@@ -160,142 +116,96 @@ end
 
 # Set defaults for bat
 if command -v bat 1>/dev/null 2>&1 and command -v rg 1>/dev/null 2>&1
-  alias bat="bat --italic-text=always --color=always --theme Nord"
-
-  function batgrep --description "Uses ripgrep instead of grep and outputs via bat"
-    rg $argv --hidden --color always | bat --paging=never
-  end
-  alias grep="batgrep"
-
-  function batfind --description "Uses ripgrep --files instead of find and outputs via bat"
-    rg $argv --ignore --color=always --smart-case --hidden --files | bat
-  end
-  alias find="batfind"
-
-  function batdiff --description "Uses bat for a nicer git diff"
-    git diff $argv --name-only --diff-filter=d | xargs bat --diff
-  end
+    alias bat="bat --italic-text=always --color=always --theme Nord"
+    alias grep="batgrep"
+    alias find="batfind"
 end
 
 # Use fzf in combination with grep
 # fzf colors
 if command -v bat 1>/dev/null 2>&1 and command -v rg 1>/dev/null 2>&1
-  alias fzf="fzf \
+    alias fzf="fzf \
     --color='fg:#d8dee9,bg:#2e3440,fg+:#81a1c1,bg+:#2e3440,border:#4c566a' \
     --color='info:#81a1c1,spinner:#b48ead,header:#bf616a,prompt:#b48ead' \
     --color='hl:#ebcb8b,hl+:#ebcb8b,pointer:#b48ead,marker:#d08770' \
     --color='fg+:reverse,header:bold,pointer:bold,marker:bold,prompt:bold' \
     --color='hl:reverse,hl+:reverse'"
-
-  set INITIAL_QUERY ""
-  set RG_PREFIX "rg --ignore --hidden --column --line-number --with-filename --no-heading --color=always --smart-case "
-  set FZF_DEFAULT_COMMAND "$RG_PREFIX '$INITIAL_QUERY'"
-  alias fg="fzf \
-    --bind 'change:reload:$RG_PREFIX {q} || true' \
-    --disabled \
-    --ansi \
-    --query '$INITIAL_QUERY' \
-    --height=50% \
-    --tabstop=2 \
-    --layout=reverse \
-    --nth=2 \
-    --delimiter : \
-    --preview-window '+{2}/2' \
-    --preview 'bat \
-    --style=numbers \
-    -r {2}: \
-    -H {2} \
-    --line-range :500 {1}'"
-  alias ff="rg \
-    --ignore \
-    --smart-case \
-    --hidden \
-    --files | fzf \
-    --ansi \
-    --height=50% \
-    --layout=reverse \
-    --tabstop=2 \
-    --preview 'bat \
-    --style=numbers \
-    --line-range :500 {}'"
 end
 
 # Forward term info in kitty when connection via ssh
-if string match -q "xterm-kitty" -- $TERM
-  alias ssh="kitty +kitten ssh"
+if string match -q xterm-kitty -- $TERM
+    alias ssh="kitty +kitten ssh"
 end
 
 # Changing "ls" to "exa"
 if command -v exa 1>/dev/null 2>&1
-  alias ls="exa -al --color=always --group-directories-first"
-  alias la="exa -a --color=always --group-directories-first"
-  alias ll="exa -l --color=always --group-directories-first"
-  alias lt="exa -aT --color=always --group-directories-first"
+    alias ls="exa -al --color=always --group-directories-first"
+    alias la="exa -a --color=always --group-directories-first"
+    alias ll="exa -l --color=always --group-directories-first"
+    alias lt="exa -aT --color=always --group-directories-first"
 end
 
 # Use nvim as default editor
 if command -v nvim 1>/dev/null 2>&1
-  alias vim="nvim"
-  alias vi="nvim"
-  # set vim as the manpager
-  set -gx MANPAGER "nvim +Man! +'set noma'"
-  set -gx EDITOR "nvim" # $EDITOR use Neovim in terminal
-end
-
+    alias vim="nvim"
+    alias vi="nvim"
+    # set vim as the manpager
+    set -gx MANPAGER "nvim +Man! +'set noma'"
+    set -gx EDITOR nvim # $EDITOR use Neovim in terminal
 end
 
 ############################################################
 # Plugins {{{1
 
 if not test -d $HOME/.config/fish/plugins
-  mkdir -p $HOME/.config/fish/plugins
+    mkdir -p $HOME/.config/fish/plugins
 end
 
 # Starship prompt
 # Change default config directory
 if not command -v starship 1>/dev/null 2>&1
-  set_color --bold red
-  echo -n "=> Error: "
-  set_color normal
-  echo -e "Starship not installed.\n"
-  set_color --bold green
-  echo -n "=> Info: "
-  set_color normal
-  echo "Please install starship through your package manager or manually. An installation guide can be found here: https://starship.rs/guide/#%F0%9F%9A%80-installation"
+    set_color --bold red
+    echo -n "=> Error: "
+    set_color normal
+    echo -e "Starship not installed.\n"
+    set_color --bold green
+    echo -n "=> Info: "
+    set_color normal
+    echo "Please install starship through your package manager or manually. An installation guide can be found here: https://starship.rs/guide/#%F0%9F%9A%80-installation"
 else
-  set -gx STARSHIP_CONFIG $HOME/.config/starship/starship.toml
-  starship init fish | source
+    set -gx STARSHIP_CONFIG $HOME/.config/starship/starship.toml
+    starship init fish | source
 end
 
 # foreign-env
 if not test -d $HOME/.config/fish/plugins/foreign-env
-  mkdir -p $HOME/.config/fish/plugins/foreign-env
-  git clone https://github.com/oh-my-fish/plugin-foreign-env $HOME/.config/fish/plugins/foreign-env
+    mkdir -p $HOME/.config/fish/plugins/foreign-env
+    git clone https://github.com/oh-my-fish/plugin-foreign-env $HOME/.config/fish/plugins/foreign-env
 end
 set fish_function_path $fish_function_path $HOME/.config/fish/plugins/foreign-env/functions
 
 # Nix
 # has to be defined after foreign-env
 if test -f /etc/profile.d/nix.sh
-  fenv source /etc/profile.d/nix.sh
+    fenv source /etc/profile.d/nix.sh
 end
 
 # pyenv
 if command -v pyenv 1>/dev/null 2>&1
-  pyenv init --path | source
-  pyenv init - | source
-  if not test -d $PYENV_ROOT/plugins/pyenv-virtualenv
-    set_color --bold red
-    echo -n "=> Error: "
-    set_color normal
-    echo -e "pyenv-virtualenv not installed.\n"
-    set_color --bold green
-    echo -n "=> Info: "
-    set_color normal
-    echo "Please install pyenv-virtualenv through your package manager or manually. An installation guide can be found here: https://github.com/pyenv/pyenv-virtualenv#installation"
-  else
-    pyenv virtualenv-init - | source
-  end
+    pyenv init --path | source
+    pyenv init - | source
+    if not test -d $PYENV_ROOT/plugins/pyenv-virtualenv
+        set_color --bold red
+        echo -n "=> Error: "
+        set_color normal
+        echo -e "pyenv-virtualenv not installed.\n"
+        set_color --bold green
+        echo -n "=> Info: "
+        set_color normal
+        echo "Please install pyenv-virtualenv through your package manager or manually. An installation guide can be found here: https://github.com/pyenv/pyenv-virtualenv#installation"
+    else
+        pyenv virtualenv-init - | source
+    end
 end
 
 # }}}1
