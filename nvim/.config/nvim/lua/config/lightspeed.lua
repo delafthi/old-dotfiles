720147215088014a93a2f61d6b0a444164340ab8
Thierry Delafontaine <deaa@zhaw.ch>
2021-12-07 16:30:21 +0100

    nvim: Removed unused option.

--- nvim/.config/nvim/lua/config/lightspeed.lua
+++ nvim/.config/nvim/lua/config/lightspeed.lua
@@ -19,7 +19,6 @@ function M.config()
     grey_out_search_area = true,
     match_only_the_start_of_same_char_seqs = true,
     limit_ft_matches = 5,
-    full_inclusive_prefix_key = "<C-x>",
     -- For instant-repeat, pressing the trigger key again (f/F/t/T)
     -- always works, but here you can specify additional keys too.
     -- By default, the values of these will be decided at runtime,
