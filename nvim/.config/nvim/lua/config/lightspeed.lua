3f2891dca5e6792a8fd6f3b0859221de6e6be7f3
Thierry Delafontaine <deaa@zhaw.ch>
2021-12-13 10:08:52 +0100

    nvim: Updated to recent changes in the plugin.

--- nvim/.config/nvim/lua/config/lightspeed.lua
+++ nvim/.config/nvim/lua/config/lightspeed.lua
@@ -11,21 +11,14 @@ function M.config()
   end
 
   lightspeed.setup({
-    jump_to_first_match = true,
-    jump_on_partial_input_safety_timeout = 400,
-    -- This can get _really_ slow if the window has a lot of content,
-    -- turn it on only if your machine can always cope with it.
-    highlight_unique_chars = false,
     grey_out_search_area = true,
+    highlight_unique_chars = false,
     match_only_the_start_of_same_char_seqs = true,
-    limit_ft_matches = 5,
-    -- For instant-repeat, pressing the trigger key again (f/F/t/T)
-    -- always works, but here you can specify additional keys too.
-    -- By default, the values of these will be decided at runtime,
-    -- based on `jump_to_first_match`.
+    jump_on_partial_input_safety_timeout = 400,
     labels = nil,
     cycle_group_fwd_key = nil,
     cycle_group_bwd_key = nil,
+    limit_ft_matches = 4,
   })
 
   vim.cmd([[
