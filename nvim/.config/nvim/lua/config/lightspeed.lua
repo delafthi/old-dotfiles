a122983ce4e456995eab3b67e7b10e180dd59be1
Thierry Delafontaine <deaa@zhaw.ch>
2022-01-14 09:13:44 +0100

    nvim: Updated configs to the recent changes of the plugin.

--- nvim/.config/nvim/lua/config/lightspeed.lua
+++ nvim/.config/nvim/lua/config/lightspeed.lua
@@ -11,10 +11,11 @@ function M.config()
   end
 
   lightspeed.setup({
-    grey_out_search_area = true,
-    highlight_unique_chars = false,
     match_only_the_start_of_same_char_seqs = true,
-    jump_on_partial_input_safety_timeout = 400,
+    jump_to_unique_chars = {
+      highlight_unique_chars = false,
+      safety_timeout = 400,
+    },
     labels = nil,
     cycle_group_fwd_key = nil,
     cycle_group_bwd_key = nil,
