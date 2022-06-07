86f751ca1e000437f661cec06dc5321fb10f3a22
Thierry Delafontaine <deaa@zhaw.ch>
2022-06-07 14:10:22 +0200

    nvim: Updated the config for the nord-nvim theme.

--- nvim/.config/nvim/lua/config/nord-nvim.lua
+++ nvim/.config/nvim/lua/config/nord-nvim.lua
@@ -6,7 +6,16 @@ function M.config()
     bold = true,
     italic = true,
     underline = true,
-    italic_comments = false,
+    italics = {
+      comments = false,
+      statements = true,
+      conditionals = true,
+      repeats = true,
+      labels = true,
+      operators = false,
+      keywords = true,
+      exceptions = true,
+    },
     cursor_line_number_background = true,
     uniform_status_lines = true, -- does not matter, because I manually overwrite it in the galaxyline config
     bold_vertical_split_line = false,
