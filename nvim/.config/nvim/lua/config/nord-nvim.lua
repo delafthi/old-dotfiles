a36575deca83409ce16168dfb4006b6ad29711b9
Thierry Delafontaine <deaa@zhaw.ch>
2022-06-10 14:47:16 +0200

    nvim: Updated the color theme config.

--- nvim/.config/nvim/lua/config/nord-nvim.lua
+++ nvim/.config/nvim/lua/config/nord-nvim.lua
@@ -3,18 +3,23 @@ local M = {}
 function M.config()
   -- Call the setup function
   require("nord").setup({
-    bold = true,
-    italic = true,
-    underline = true,
-    italics = {
-      comments = false,
-      statements = true,
-      conditionals = true,
-      repeats = true,
-      labels = true,
-      operators = false,
-      keywords = true,
-      exceptions = true,
+    bold = {
+      enabled = true,
+    },
+    italic = {
+      enabled = true,
+      comment = false,
+      boolean = true,
+      statement = true,
+      conditional = true,
+      ["repeat"] = true,
+      label = true,
+      operator = false,
+      keyword = true,
+      exception = true,
+    },
+    underline = {
+      enabled = true,
     },
     cursor_line_number_background = true,
     uniform_status_lines = true, -- does not matter, because I manually overwrite it in the galaxyline config
