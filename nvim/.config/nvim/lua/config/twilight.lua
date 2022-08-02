999c3c20f8eb7a3d13e9382bc9a818bf18033f3f
Thierry Delafontaine <deaa@zhaw.ch>
2022-08-02 11:14:39 +0200

    nvim: Minor changes to zen-mode and twillight

--- nvim/.config/nvim/lua/config/twilight.lua
+++ nvim/.config/nvim/lua/config/twilight.lua
@@ -5,10 +5,10 @@ function M.config()
     dimming = {
       alpha = 0.25, -- amount of dimming
       -- we try to get the foreground from the highlight groups or fallback color
-      color = { "Normal", "#ffffff" },
+      color = { "Normal", "#d8dee9" },
       inactive = false, -- when true, other windows will be fully dimmed (unless they contain the same buffer)
     },
-    context = 10, -- amount of lines we will try to show around the current line
+    context = 8, -- amount of lines we will try to show around the current line
     treesitter = true, -- use treesitter when available for the filetype
     -- treesitter is used to automatically expand the visible text,
     -- but you can further control the types of nodes that should always be fully expanded
