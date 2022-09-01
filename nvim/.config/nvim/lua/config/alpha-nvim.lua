8c1b64ed539bfd83b33645252a024285d0f513fd
Thierry Delafontaine <delafontaineth@pm.me>
2022-09-01 11:49:44 +0200

    nvim: Set hlgroups in alpha dashboard

--- nvim/.config/nvim/lua/config/alpha-nvim.lua
+++ nvim/.config/nvim/lua/config/alpha-nvim.lua
@@ -1,54 +1,141 @@
 local M = {}
 
 function M.config()
-  local alpha = require("alpha")
-  local dashboard = require("alpha.themes.dashboard")
-  -- Set header
-  dashboard.section.header.val = {
-    "                                                     ",
-    "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
-    "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
-    "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
-    "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
-    "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
-    "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
-    "                                                     ",
-  }
+  local leader = "SPC"
 
-  -- Set menu
-  dashboard.section.buttons.val = {
-    dashboard.button("SPC f n", "  > New File", ":enew<Cr>", {}),
-    dashboard.button(
-      "SPC w s l",
-      "  > Restore last session",
-      ":lua require('persistence').load({last = true})<Cr>",
-      {}
-    ),
-    dashboard.button(
-      "SPC f r",
-      "  > Recently opened files",
-      ":Telescope oldfiles<Cr>",
-      {}
-    ),
-    dashboard.button(
-      "SPC f f",
-      "  > Find File",
-      ":Telescope find_files<Cr>",
-      {}
-    ),
-    dashboard.button(
-      "SPC f b",
-      "  > File Browser",
-      ":Telescope find_browser<Cr>",
-      {}
-    ),
-    dashboard.button(
-      "SPC s g",
-      "  > Find Word",
-      ":Telescope live_grep<Cr>",
-      {}
-    ),
-    dashboard.button("SPC f n", "  > New File", ":enew<Cr>", {}),
+  local function button(sc, txt, keybind, keybind_opts, opts)
+    local sc_ = sc:gsub("%s", ""):gsub(leader, "<leader>")
+
+    opts.position = opts.position or "center"
+    opts.shortcut = opts.shortcut or sc
+    opts.cursor = opts.cursor or 5
+    opts.width = opts.width or 50
+    opts.align_shortcut = opts.align_shortcut or "right"
+    opts.hl_shortcut = opts.hl_shortcut or "Keyword"
+
+    if keybind then
+      keybind_opts = keybind_opts
+        or { noremap = true, silent = true, nowait = true }
+      opts.keymap = { "n", sc_, keybind, keybind_opts }
+    end
+
+    local function on_press()
+      local key = vim.api.nvim_replace_termcodes(
+        keybind or sc_ .. "<Ignore>",
+        true,
+        false,
+        true
+      )
+      vim.api.nvim_feedkeys(key, "tx", false)
+    end
+
+    return {
+      type = "button",
+      val = txt,
+      on_press = on_press,
+      opts = opts,
+    }
+  end
+
+  local alpha = require("alpha")
+  local dashboard = {
+    config = {
+      layout = {
+        {
+          type = "padding",
+          val = 2,
+        },
+        {
+          type = "text",
+          val = {
+            "                                                     ",
+            "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+            "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+            "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+            "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+            "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+            "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+            "                                                     ",
+          },
+          opts = {
+            position = "center",
+            hl = "DashboardHeader",
+          },
+        },
+        {
+          type = "padding",
+          val = 2,
+        },
+        {
+          type = "group",
+          val = {
+            button(
+              "SPC f n",
+              "  > New File",
+              ":enew<Cr>",
+              {},
+              { hl_shortcut = "DashboardShortCut" }
+            ),
+            button(
+              "SPC w s l",
+              "  > Restore last session",
+              ":lua require('persistence').load({last = true})<Cr>",
+              {},
+              { hl_shortcut = "DashboardShortCut" }
+            ),
+            button(
+              "SPC f r",
+              "  > Recently opened files",
+              ":Telescope oldfiles<Cr>",
+              {},
+              { hl_shortcut = "DashboardShortCut" }
+            ),
+            button(
+              "SPC f f",
+              "  > Find File",
+              ":Telescope find_files<Cr>",
+              {},
+              { hl_shortcut = "DashboardShortCut" }
+            ),
+            button(
+              "SPC f b",
+              "  > File Browser",
+              ":Telescope find_browser<Cr>",
+              {},
+              { hl_shortcut = "DashboardShortCut" }
+            ),
+            button(
+              "SPC s g",
+              "  > Find Word",
+              ":Telescope live_grep<Cr>",
+              {},
+              { hl_shortcut = "DashboardShortCut" }
+            ),
+            button(
+              "SPC f n",
+              "  > New File",
+              ":enew<Cr>",
+              {},
+              { hl_shortcut = "DashboardShortCut" }
+            ),
+          },
+          opts = {
+            spacing = 1,
+          },
+        },
+        {
+          type = "text",
+          val = "",
+          opts = {
+            position = "center",
+            hl = "DashboardFooter",
+          },
+        },
+      },
+    },
+    opts = {
+      margin = 5,
+    },
   }
 
   -- Disable folding on alpha buffer
