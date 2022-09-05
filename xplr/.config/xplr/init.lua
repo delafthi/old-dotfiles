f906ec7ff3424d4f62b65c3fc33920428dc9148c
Thierry Delafontaine <delafontaineth@pm.me>
2022-09-05 20:11:14 +0200

    xplr: Started custumizing the extensive configuration.

--- xplr/.config/xplr/init.lua
+++ xplr/.config/xplr/init.lua
@@ -2,188 +2,34 @@
 local xplr = xplr -- The globally exposed configuration to be overridden.
 ---@diagnostic enable
 
--- This is the built-in configuration file that gets loaded and sets the
--- default values when xplr loads, before loading any other custom
--- configuration file.
---
--- You can use this file as a reference to create a your custom config file.
---
--- To create a custom configuration file, you need to define the script version
--- for compatibility checks.
---
--- See https://xplr.dev/en/upgrade-guide
---
--- ```lua
--- version = "0.0.0"
--- ```
+version = "0.19.0"
 
--- # Configuration ------------------------------------------------------------
---
--- xplr can be configured using [Lua][1] via a special file named `init.lua`,
--- which can be placed in `~/.config/xplr/` (local to user) or `/etc/xplr/`
--- (global) depending on the use case.
---
--- When xplr loads, it first executes the [built-in init.lua][2] to set the
--- default values, which is then overwritten by another config file, if found
--- using the following lookup order:
---
--- 1. `--config /path/to/init.lua`
--- 2. `~/.config/xplr/init.lua`
--- 3. `/etc/xplr/init.lua`
---
--- The first one found will be loaded by xplr and the lookup will stop.
---
--- The loaded config can be further extended using the `-C` or `--extra-config`
--- command-line option.
---
---
--- [1]: https://www.lua.org
--- [2]: https://github.com/sayanarijit/xplr/blob/main/src/init.lua
--- [3]: https://xplr.dev/en/upgrade-guide
-
--- ## Config ------------------------------------------------------------------
---
--- The xplr configuration, exposed via `xplr.config` Lua API contains the
--- following sections.
---
--- See:
---
--- * [xplr.config.general](https://xplr.dev/en/general-config)
--- * [xplr.config.node_types](https://xplr.dev/en/node_types)
--- * [xplr.config.layouts](https://xplr.dev/en/layouts)
--- * [xplr.config.modes](https://xplr.dev/en/modes)
-
--- ### General Configuration --------------------------------------------------
---
--- The general configuration properties are grouped together in
--- `xplr.config.general`.
-
--- Set it to `true` if you want to ignore the startup errors. You can still see
--- the errors in the logs.
---
--- Type: boolean
-xplr.config.general.disable_debug_error_mode = false
-
--- Set it to `true` if you want to enable mouse scrolling.
---
--- Type: boolean
-xplr.config.general.enable_mouse = false
-
--- Set it to `true` to show hidden files by default.
---
--- Type: boolean
-xplr.config.general.show_hidden = false
-
--- Set it to `true` to use only a subset of selected operations that forbids
--- executing commands or performing write operations on the file-system.
---
--- Type: boolean
-xplr.config.general.read_only = false
-
--- Set it to `true` if you want to enable a safety feature that will save you
--- from yourself when you type recklessly.
---
--- Type: boolean
-xplr.config.general.enable_recover_mode = false
-
--- Set it to `true` if you want to hide all remaps in the help menu.
---
--- Type: boolean
-xplr.config.general.hide_remaps_in_help_menu = false
-
--- Set it to `true` if you want the cursor to stay in the same position when
--- the focus is on the first path and you navigate to the previous path
--- (by pressing `up`/`k`), or when the focus is on the last path and you
--- navigate to the next path (by pressing `down`/`j`).
--- The default behavior is to rotate from the last/first path.
---
--- Type: boolean
-xplr.config.general.enforce_bounded_index_navigation = false
-
--- This is the shape of the prompt for the input buffer.
---
--- Type: nullable string
-xplr.config.general.prompt.format = "❯ "
-
--- This is the style of the prompt for the input buffer.
---
--- Type: [Style](https://xplr.dev/en/style)
+-- General Configuration
+-- ~~~~~~~~~~~~~~~~~~~~~
+xplr.config.general.show_hidden = true
+xplr.config.general.prompt.format = "> "
 xplr.config.general.prompt.style = {}
-
--- The string to indicate an information in logs.
---
--- Type: nullable string
-xplr.config.general.logs.info.format = "INFO"
-
--- The style for the informations logs.
---
--- Type: [Style](https://xplr.dev/en/style)
-xplr.config.general.logs.info.style = { fg = "LightBlue" }
-
--- The string to indicate an success in logs.
---
--- Type: nullable string
+xplr.config.general.logs.info.style = { fg = "White" }
 xplr.config.general.logs.success.format = "SUCCESS"
-
--- The style for the success logs.
---
--- Type: [Style](https://xplr.dev/en/style)
 xplr.config.general.logs.success.style = { fg = "Green" }
-
--- The string to indicate an warnings in logs.
---
--- Type: nullable string
 xplr.config.general.logs.warning.format = "WARNING"
-
--- The style for the warnings logs.
---
--- Type: [Style](https://xplr.dev/en/style)
 xplr.config.general.logs.warning.style = { fg = "Yellow" }
-
--- The string to indicate an error in logs.
---
--- Type: nullable string
 xplr.config.general.logs.error.format = "ERROR"
-
--- The style for the error logs.
---
--- Type: [Style](https://xplr.dev/en/style)
 xplr.config.general.logs.error.style = { fg = "Red" }
-
--- Columns to display in the table header.
---
--- Type: nullable list of tables with the following fields:
---
--- * format: nullable string
--- * style: [Style](https://xplr.dev/en/style)
 xplr.config.general.table.header.cols = {
-  { format = " index", style = {} },
-  { format = "╭─── path", style = {} },
+  { format = "index", style = { fg = "DarkGray" } },
+  { format = "path", style = {} },
   { format = "permissions", style = {} },
   { format = "size", style = {} },
   { format = "modified", style = {} },
 }
-
--- Style of the table header.
---
--- Type: [Style](https://xplr.dev/en/style)
-xplr.config.general.table.header.style = {}
-
--- Height of the table header.
---
--- Type: nullable integer
+--[[ xplr.config.general.table.header.style = ]]
+--[[   { fg = "White", add_modifiers = { "Bold" } } ]]
 xplr.config.general.table.header.height = 1
-
--- Columns to display in each row in the table.
---
--- Type: nullable list of tables with the following fields:
---
--- * format: nullable string
--- * style: [Style](https://xplr.dev/en/style)
 xplr.config.general.table.row.cols = {
   {
     format = "builtin.fmt_general_table_row_cols_0",
-    style = {},
+    style = { fg = "DarkGray", sub_modifiers = { "Bold", "Italic" } },
   },
   {
     format = "builtin.fmt_general_table_row_cols_1",
@@ -202,123 +48,35 @@ xplr.config.general.table.row.cols = {
     style = {},
   },
 }
-
--- Default style of the table.
---
--- Type: [Style](https://xplr.dev/en/style)
-xplr.config.general.table.row.style = {}
-
--- Height of the table rows.
---
--- Type: nullable integer
-xplr.config.general.table.row.height = 0
-
--- Default style of the table.
---
--- Type: [Style](https://xplr.dev/en/style)
-xplr.config.general.table.style = {}
-
--- Tree to display in the table.
---
--- Type: nullable list of tables with the following fields:
---
--- * format: nullable string
--- * style: [Style](https://xplr.dev/en/style)
 xplr.config.general.table.tree = {
-  { format = "├", style = {} },
-  { format = "├", style = {} },
-  { format = "╰", style = {} },
+  { format = "", style = {} },
+  { format = "", style = {} },
+  { format = "", style = {} },
 }
-
--- Spacing between the columns in the table.
---
--- Type: nullable integer
 xplr.config.general.table.col_spacing = 1
-
--- Constraint for the column widths.
---
--- Type: nullable list of [Constraint](https://xplr.dev/en/layouts#constraint)
 xplr.config.general.table.col_widths = {
-  { Percentage = 10 },
+  { Length = 8 },
   { Percentage = 50 },
   { Percentage = 10 },
   { Percentage = 10 },
   { Percentage = 20 },
 }
-
--- The content that is placed before the item name for each row by default.
---
--- Type: nullable string
-xplr.config.general.default_ui.prefix = "─ "
-
--- The content which is appended to each item name for each row by default.
---
--- Type: nullable string
+xplr.config.general.default_ui.prefix = ""
 xplr.config.general.default_ui.suffix = ""
-
--- The default style of each item for each row.
---
--- Type: [Style](https://xplr.dev/en/style)
-xplr.config.general.default_ui.style = {}
-
--- The string placed before the item name for a focused row.
---
--- Type: nullable string
-xplr.config.general.focus_ui.prefix = "▸["
-
--- The string placed after the item name for a focused row.
---
--- Type: nullable string
-xplr.config.general.focus_ui.suffix = "]"
-
--- Style for focused item.
--- Type: [Style](https://xplr.dev/en/style)
+xplr.config.general.focus_ui.prefix = "> "
+xplr.config.general.focus_ui.suffix = ""
 xplr.config.general.focus_ui.style = { add_modifiers = { "Bold" } }
-
--- The string placed before the item name for a selected row.
---
--- Type: nullable string
-xplr.config.general.selection_ui.prefix = "─{"
-
--- The string placed after the item name for a selected row.
---
--- Type: nullable string
-xplr.config.general.selection_ui.suffix = "}"
-
--- Style for selected rows.
---
--- Type: [Style](https://xplr.dev/en/style)
+xplr.config.general.selection_ui.prefix = "+"
+xplr.config.general.selection_ui.suffix = ""
 xplr.config.general.selection_ui.style = {
-  fg = "LightGreen",
   add_modifiers = { "Bold" },
 }
-
--- The string placed before item name for a selected row that gets the focus.
---
--- Type: nullable string
-xplr.config.general.focus_selection_ui.prefix = "▸["
-
--- The string placed after the item name for a selected row that gets the focus.
---
--- Type: nullable string
-xplr.config.general.focus_selection_ui.suffix = "]"
-
--- Style for a selected row that gets the focus.
---
--- Type: [Style](https://xplr.dev/en/style)
+xplr.config.general.focus_selection_ui.prefix = ">+"
+xplr.config.general.focus_selection_ui.suffix = ""
 xplr.config.general.focus_selection_ui.style = {
-  fg = "LightGreen",
   add_modifiers = { "Bold" },
 }
-
--- The shape of the separator for the Sort & filter panel.
---
--- Type: nullable string
-xplr.config.general.sort_and_filter_ui.separator.format = " › "
-
--- The style of the separator for the Sort & filter panel.
---
--- Type: [Style](https://xplr.dev/en/style)
+xplr.config.general.sort_and_filter_ui.separator.format = "% "
 xplr.config.general.sort_and_filter_ui.separator.style = {
   add_modifiers = { "Dim" },
 }
@@ -336,22 +94,26 @@ xplr.config.general.sort_and_filter_ui.default_identifier.style = {}
 -- The shape of the forward direction indicator for sort identifiers in Sort & filter panel.
 --
 -- Type: nullable string
-xplr.config.general.sort_and_filter_ui.sort_direction_identifiers.forward.format = "↓"
+xplr.config.general.sort_and_filter_ui.sort_direction_identifiers.forward.format =
+  "↓"
 
 -- Style of forward direction indicator in Sort & filter panel.
 --
 -- Type: [Style](https://xplr.dev/en/style)
-xplr.config.general.sort_and_filter_ui.sort_direction_identifiers.forward.style = nil
+xplr.config.general.sort_and_filter_ui.sort_direction_identifiers.forward.style =
+  nil
 
 -- The shape of the reverse direction indicator for sort identifiers in Sort & filter panel.
 --
 -- Type: nullable string
-xplr.config.general.sort_and_filter_ui.sort_direction_identifiers.reverse.format = "↑"
+xplr.config.general.sort_and_filter_ui.sort_direction_identifiers.reverse.format =
+  "↑"
 
 -- Style of reverse direction indicator in Sort & filter panel.
 --
 -- Type: [Style](https://xplr.dev/en/style)
-xplr.config.general.sort_and_filter_ui.sort_direction_identifiers.reverse.style = nil
+xplr.config.general.sort_and_filter_ui.sort_direction_identifiers.reverse.style =
+  nil
 
 -- The identifiers used to denote applied sorters in the Sort & filter panel.
 --
@@ -709,7 +471,7 @@ xplr.config.node_types.directory.style = {
 -- xplr.config.node_types.directory.meta.foo = "foo"
 -- xplr.config.node_types.directory.meta.bar = "bar"
 -- ```
-xplr.config.node_types.directory.meta.icon = "ð"
+xplr.config.node_types.directory.meta.icon = " "
 
 -- The style for the file nodes.
 --
@@ -727,7 +489,7 @@ xplr.config.node_types.file.style = {}
 -- xplr.config.node_types.file.meta.foo = "foo"
 -- xplr.config.node_types.file.meta.bar = "bar"
 -- ```
-xplr.config.node_types.file.meta.icon = "ƒ"
+xplr.config.node_types.file.meta.icon = " "
 
 -- The style for the symlink nodes.
 --
@@ -748,7 +510,7 @@ xplr.config.node_types.symlink.style = {
 -- xplr.config.node_types.symlink.meta.foo = "foo"
 -- xplr.config.node_types.symlink.meta.bar = "bar"
 -- ```
-xplr.config.node_types.symlink.meta.icon = "§"
+xplr.config.node_types.symlink.meta.icon = " "
 
 -- Metadata and style based on mime types.
 -- It is possible to use the wildcard `*` to match all mime sub types. It will
