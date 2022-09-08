0695405f366d8a9ddda5e16685fc6b1856944059
Thierry Delafontaine <deaa@zhaw.ch>
2022-09-08 14:44:06 +0200

    nvim: Updated default config. Disabled fat headers.

--- nvim/.config/nvim/lua/config/headlines.lua
+++ nvim/.config/nvim/lua/config/headlines.lua
@@ -4,22 +4,126 @@ function M.config()
   -- Call the setup function
   require("headlines").setup({
     markdown = {
-      source_pattern_start = "^```",
-      source_pattern_end = "^```$",
-      dash_pattern = "^---+$",
-      headline_pattern = "^#+",
-      headline_signs = { "Headline" },
-      codeblock_sign = "CodeBlock",
+      query = vim.treesitter.parse_query(
+        "markdown",
+        [[
+                (atx_heading [
+                    (atx_h1_marker)
+                    (atx_h2_marker)
+                    (atx_h3_marker)
+                    (atx_h4_marker)
+                    (atx_h5_marker)
+                    (atx_h6_marker)
+                ] @headline)
+
+                (thematic_break) @dash
+
+                (fenced_code_block) @codeblock
+
+                (block_quote_marker) @quote
+                (block_quote (paragraph (inline (block_continuation) @quote)))
+            ]]
+      ),
+      headline_highlights = { "Headline" },
+      codeblock_highlight = "CodeBlock",
       dash_highlight = "Dash",
+      dash_string = "-",
+      quote_highlight = "Quote",
+      quote_string = "┃",
+      fat_headlines = false,
     },
     rmd = {
-      source_pattern_start = "^```",
-      source_pattern_end = "^```$",
-      dash_pattern = "^---+$",
-      headline_pattern = "^#+",
-      headline_signs = { "Headline" },
-      codeblock_sign = "CodeBlock",
+      query = vim.treesitter.parse_query(
+        "markdown",
+        [[
+                (atx_heading [
+                    (atx_h1_marker)
+                    (atx_h2_marker)
+                    (atx_h3_marker)
+                    (atx_h4_marker)
+                    (atx_h5_marker)
+                    (atx_h6_marker)
+                ] @headline)
+
+                (thematic_break) @dash
+
+                (fenced_code_block) @codeblock
+
+                (block_quote_marker) @quote
+                (block_quote (paragraph (inline (block_continuation) @quote)))
+            ]]
+      ),
+      treesitter_language = "markdown",
+      headline_highlights = { "Headline" },
+      codeblock_highlight = "CodeBlock",
       dash_highlight = "Dash",
+      dash_string = "-",
+      quote_highlight = "Quote",
+      quote_string = "┃",
+      fat_headlines = false,
+    },
+    norg = {
+      query = vim.treesitter.parse_query(
+        "norg",
+        [[
+                [
+                    (heading1_prefix)
+                    (heading2_prefix)
+                    (heading3_prefix)
+                    (heading4_prefix)
+                    (heading5_prefix)
+                    (heading6_prefix)
+                ] @headline
+
+                (weak_paragraph_delimiter) @dash
+                (strong_paragraph_delimiter) @doubledash
+
+                ((ranged_tag
+                    name: (tag_name) @_name
+                    (#eq? @_name "code")
+                ) @codeblock (#offset! @codeblock 0 0 1 0))
+
+                (quote1_prefix) @quote
+            ]]
+      ),
+      headline_highlights = { "Headline" },
+      codeblock_highlight = "CodeBlock",
+      dash_highlight = "Dash",
+      dash_string = "-",
+      doubledash_highlight = "DoubleDash",
+      doubledash_string = "=",
+      quote_highlight = "Quote",
+      quote_string = "┃",
+      fat_headlines = false,
+    },
+    org = {
+      query = vim.treesitter.parse_query(
+        "org",
+        [[
+                (headline (stars) @headline)
+
+                (
+                    (expr) @dash
+                    (#match? @dash "^-----+$")
+                )
+
+                (block
+                    name: (expr) @_name
+                    (#eq? @_name "SRC")
+                ) @codeblock
+
+                (paragraph . (expr) @quote
+                    (#eq? @quote ">")
+                )
+            ]]
+      ),
+      headline_highlights = { "Headline" },
+      codeblock_highlight = "CodeBlock",
+      dash_highlight = "Dash",
+      dash_string = "-",
+      quote_highlight = "Quote",
+      quote_string = "┃",
+      fat_headlines = false,
     },
   })
 end
