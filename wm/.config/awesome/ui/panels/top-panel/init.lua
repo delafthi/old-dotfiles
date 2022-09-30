a24c1f877f418d060fd8e14cf4b731dcbf5983ce
Thierry Delafontaine <deaa@zhaw.ch>
2022-09-30 13:08:23 +0200

    fix(wm): Moved taglist buttons into definition. Removed duplicate definition of mod

--- wm/.config/awesome/ui/panels/top-panel/init.lua
+++ wm/.config/awesome/ui/panels/top-panel/init.lua
@@ -26,31 +26,6 @@ return function(s)
   --- Animated tag list
   --- ~~~~~~~~~~~~~~~~~
 
-  --- Taglist buttons
-  local modkey = "Mod4"
-  local taglist_buttons = gears.table.join(
-    awful.button({}, 1, function(t)
-      t:view_only()
-    end),
-    awful.button({ modkey }, 1, function(t)
-      if client.focus then
-        client.focus:move_to_tag(t)
-      end
-    end),
-    awful.button({}, 3, awful.tag.viewtoggle),
-    awful.button({ modkey }, 3, function(t)
-      if client.focus then
-        client.focus:toggle_tag(t)
-      end
-    end),
-    awful.button({}, 4, function(t)
-      awful.tag.viewnext(t.screen)
-    end),
-    awful.button({}, 5, function(t)
-      awful.tag.viewprev(t.screen)
-    end)
-  )
-
   local function tag_list(s)
     local taglist = awful.widget.taglist({
       screen = s,
@@ -117,7 +92,30 @@ return function(s)
           end
         end,
       },
-      buttons = taglist_buttons,
+      buttons = {
+        awful.button({}, 1, function(t)
+          t:view_only()
+        end),
+        awful.button({ mod }, 1, function(t)
+          if client.focus then
+            client.focus:move_to_tag(t)
+          end
+        end),
+        awful.button({}, 3, function()
+          awful.tag.viewtoggle()
+        end),
+        awful.button({ mod }, 3, function(t)
+          if client.focus then
+            client.focus:toggle_tag(t)
+          end
+        end),
+        awful.button({}, 4, function(t)
+          awful.tag.viewnext(t.screen)
+        end),
+        awful.button({}, 5, function(t)
+          awful.tag.viewprev(t.screen)
+        end),
+      },
     })
 
     local widget = widgets.button.elevated.state({
