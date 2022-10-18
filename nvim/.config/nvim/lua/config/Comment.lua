1c734973f0c15834d8b5c96d8a1a16bf6ab1a9e1
Thierry Delafontaine <deaa@zhaw.ch>
2022-10-18 15:50:33 +0200

    fix(nvim): Updated config

--- nvim/.config/nvim/lua/config/Comment.lua
+++ nvim/.config/nvim/lua/config/Comment.lua
@@ -4,40 +4,26 @@ function M.config()
   -- Call the setup function
   require("Comment").setup({
     ---Add a space b/w comment and the line
-    ---@type boolean
     padding = true,
-
     ---Whether the cursor should stay at its position
-    ---NOTE: This only affects NORMAL mode mappings and doesn't work with dot-repeat
-    ---@type boolean
     sticky = true,
-
-    ---Lines to be ignored while comment/uncomment.
-    ---Could be a regex string or a function that returns a regex string.
-    ---Example: Use '^$' to ignore empty lines
-    ---@type string|fun():string
+    ---Lines to be ignored while (un)comment
     ignore = nil,
-
-    ---LHS of toggle mappings in NORMAL + VISUAL mode
-    ---@type table
+    ---LHS of toggle mappings in NORMAL mode
     toggler = {
       ---Line-comment toggle keymap
       line = "gcc",
       ---Block-comment toggle keymap
       block = "gbc",
     },
-
-    ---LHS of operator-pending mappings in NORMAL + VISUAL mode
-    ---@type table
+    ---LHS of operator-pending mappings in NORMAL and VISUAL mode
     opleader = {
       ---Line-comment keymap
       line = "gc",
       ---Block-comment keymap
       block = "gb",
     },
-
     ---LHS of extra mappings
-    ---@type table
     extra = {
       ---Add comment on the line above
       above = "gcO",
@@ -46,46 +32,19 @@ function M.config()
       ---Add comment at the end of line
       eol = "gcA",
     },
-
-    ---Create basic (operator-pending) and extended mappings for NORMAL + VISUAL mode
-    ---@type table
+    ---Enable keybindings
+    ---NOTE: If given `false` then the plugin won't create any mappings
     mappings = {
-      ---Operator-pending mapping
-      ---Includes `gcc`, `gbc`, `gc[count]{motion}` and `gb[count]{motion}`
-      ---NOTE: These mappings can be changed individually by `opleader` and `toggler` config
+      ---Operator-pending mapping; `gcc` `gbc` `gc[count]{motion}` `gb[count]{motion}`
       basic = true,
-      ---Extra mapping
-      ---Includes `gco`, `gcO`, `gcA`
+      ---Extra mapping; `gco`, `gcO`, `gcA`
       extra = true,
-      ---Extended mapping
-      ---Includes `g>`, `g<`, `g>[count]{motion}` and `g<[count]{motion}`
-      extended = false,
     },
 
-    ---Pre-hook, called before commenting the line
-    ---@type fun(ctx: Ctx):string
-    pre_hook = function(ctx)
-      -- Only calculate commentstring for tsx filetypes
-      local U = require("Comment.utils")
+    ---Function to call before (un)comment
+    pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
 
-      -- Determine the location where to calculate commentstring from
-      local location = nil
-      if ctx.ctype == U.ctype.block then
-        location =
-          require("ts_context_commentstring.utils").get_cursor_location()
-      elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-        location =
-          require("ts_context_commentstring.utils").get_visual_start_location()
-      end
-
-      return require("ts_context_commentstring.internal").calculate_commentstring({
-        key = ctx.ctype == U.ctype.line and "__default" or "__multiline",
-        location = location,
-      })
-    end,
-
-    ---Post-hook, called after commenting is done
-    ---@type fun(ctx: Ctx)
+    ---Function to call after (un)comment
     post_hook = nil,
   })
 end
